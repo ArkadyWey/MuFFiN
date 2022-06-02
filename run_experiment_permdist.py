4d451d02372f525448529c6cb0ea6356e3a4bc88
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-06-02 17:12:42 +0100

    Add run module for permeability distributions with cell size calculation

---
+++ run_experiment_permdist.py
@@ -0,0 +1,46 @@
+import numpy
+import os
+
+import run_preprocess_2D
+import configure
+
+# Parameters 
+# ------
+num_reps = 10_000 # number of times to repeat a test
+
+num_nodes_list = [1] # List of num nodes in cells to get distribution for
+
+num_tests = len(num_nodes_list) # Nuber of different cell sizes to test
+
+
+
+# Main
+# -----
+perm_effe_2 = numpy.zeros(shape=(num_tests, num_reps)) # place to store result
+# perm_effe_2[t,r] = effective permeability for repeat repeats[r] of test tests[t]
+
+for t in range(num_tests):
+    for r in range(num_reps):  
+        
+        # Get effective permeability in 0,0 direction
+        # -----
+        perm_3, depo_2, conc_max_disc_1 = run_preprocess_2D.main(configure=configure)
+
+        perm_effe = perm_3[0,0,0]
+
+        perm_effe_2[t,r] = perm_effe
+
+
+
+# Save results 
+# -----
+path_results = os.path.join(".","results_experiment_permdist")
+if not os.path.exists(path_results):
+    os.mkdir(path_results)
+    
+numpy.save(file=os.path.join(path_results,"perm_effe_2.npy"), arr=perm_effe_2, allow_pickle=True, fix_imports=True)
+
+
+
+
+
