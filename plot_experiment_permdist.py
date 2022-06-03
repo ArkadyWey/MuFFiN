e4928b78d6bfbcbd68233ebf3da0a98bd62ada76
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-06-03 03:34:38 +0100

    Add plots to  permdist experiment
    - Now plot script plots one graph for N=1 case, and
    one for all N.
    - The N=1 grpah compares histogram to the known distribution and
    shows they are similar as num reps tends to infinity.
    - The second graoh shows the histogram for each N on top of each other.
    This might get busy and so consider taking a subset of n.
    
    Future commits
    - Plot the mean and standard deviation of the histograms
    as a function of N. Compare this plot
    to Jon's paper and use this to determine whether this code is right.
    - If this is same as paper then code up to here is correct
    and can be used for dynamic experiments.

--- plot_experiment_permdist.py
+++ plot_experiment_permdist.py
@@ -16,30 +16,47 @@ perm_effe_2 = numpy.load(os.path.join(path_results, "perm_effe_2.npy"))
 
 # Plot permeability histogram 
 # -----
-fig, ax = plt.subplots(1,1)
 
 num_tests = len(perm_effe_2[:,0])
 num_reps  = len(perm_effe_2[0,:])
 
-for t in range(num_tests): 
-    count, bins, ignored = ax.hist(x=perm_effe_2[t,:], bins=50, density=True, align='mid', label=r"$N=1$")
-
-ax.set_xlabel(r"$k^{00}$")
-ax.set_ylabel(r"Probability density")
+num_nodes_list = [1,4]
 
 
-# Compare with log-normal distribution that edges are drawn from 
+
+
+# Plot histogram for N=1 (outside to get bins for pdf)
+# -----
+fig, ax = plt.subplots(1,1)
+
+count, bins_1, ignored = ax.hist(x=perm_effe_2[0,:], bins=50, density=True, align='mid', label=r"$N=1$", alpha=0.4)
+# ...and compare with log-normal distribution that edges are drawn from 
 # -----
 import configure
-mu = configure.Parameters().mean
-sigma = configure.Parameters().sd
+mu = configure.Configure(num_nodes=1).mean
+sigma = configure.Configure(num_nodes=1).sd
 
-x = numpy.linspace(min(bins), max(bins), 1_000)
+x = numpy.linspace(min(bins_1), max(bins_1), 1_000)
 
 pdf = (numpy.exp(-(numpy.log(x) - mu)**2 / (2 * sigma**2))  / (x * sigma * numpy.sqrt(2 * numpy.pi))) 
 
 ax.plot(x, pdf, linewidth=2, color='r', label=r"pdf")
 
+ax.legend()
+plt.savefig(fname=os.path.join(path_results,"prob_density__v__perm_N=1.svg"), format="svg")
+
+
+
+
+# Plot histogram for all N on same graph
+# -----
+fig, ax = plt.subplots(1,1)
+
+for t in range(num_tests):
+    count, bins, ignored = ax.hist(x=perm_effe_2[t,:], bins=50, density=True, align='mid', label=r"$N={}$".format(num_nodes_list[t]), alpha=0.4)
+
+ax.set_xlabel(r"$k^{00}$")
+ax.set_ylabel(r"Probability density")
 ax.legend()
 
 plt.savefig(fname=os.path.join(path_results,"prob_density__v__perm.svg"), format="svg")
