ffb86fbf605249d07d2c3a296a10798e664330ae
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-06-03 02:59:11 +0100

    Add manual 4 node grid initial condition
    - At the moment inputting values on each edge
    manually.
    - This will not be practical for larger graphs, which we need.
    - Therefore, future commits will replace this
    function with one that fills a grid with lognormal
    random conductances
    automatically.

--- initial_conditions_2D.py
+++ initial_conditions_2D.py
@@ -77,7 +77,7 @@ def grid_prescribed(num_nodes: int, num_refs: int):
 def grid_log_normal(num_nodes: int, num_refs: int, mean: float, sd: float):
     """
     - num_nodes: int
-        Number of nodes in the cell.
+        Number of nodes in the cell. Must be square number.
     num_refs: int
         Number of lengths in the reference set. 
         For example, if reference set is {-1,0,+1} then num_refs==3.
@@ -90,23 +90,60 @@ def grid_log_normal(num_nodes: int, num_refs: int, mean: float, sd: float):
     
     # Define parameters 
     # -----
-    
     cond_init_4 = numpy.zeros(shape=(num_nodes, num_nodes, num_refs, num_refs))
-    
-    
-    if num_nodes == 1:
+    num_unique_edges = int(2*num_nodes)
+    samples = numpy.random.lognormal(mean=mean, sigma=sd, size=num_unique_edges)
 
-        edge_hori = numpy.random.lognormal(mean=mean, sigma=sd, size=None)
-        edge_vert = numpy.random.lognormal(mean=mean, sigma=sd, size=None)
-        # Grid of one node
-        # ----------------
-        cond_init_4[0,0,+1,0] = edge_hori
-        cond_init_4[0,0,-1,0] = edge_hori
+    if num_nodes == 1:     
+               
+        # External edges
+        # ------
+        cond_init_4[0,0,+1,0] = samples[0]
+        cond_init_4[0,0,-1,0] = samples[0]
 
-        cond_init_4[0,0,0,+1] = edge_vert
-        cond_init_4[0,0,0,-1] = edge_vert
+        cond_init_4[0,0,0,+1] = samples[1]
+        cond_init_4[0,0,0,-1] = samples[1]
+
+        
     
+
     elif num_nodes == 4: 
-        pass
-    
+        
+        # External edges
+        # ------
+        
+        # Horizontal 
+        cond_init_4[0,1,-1,0] = samples[0]
+        cond_init_4[1,0,+1,0] = samples[0]
+        
+        cond_init_4[2,3,-1,0] = samples[1]
+        cond_init_4[3,2,+1,0] = samples[1]
+
+        # Vertical
+        cond_init_4[0,2,0,+1] = samples[2]
+        cond_init_4[2,0,0,-1] = samples[2]
+        
+        cond_init_4[1,3,0,+1] = samples[3]
+        cond_init_4[3,1,0,-1] = samples[3]
+
+
+        # Internal edges
+        # ------
+
+        # Horizontal
+        cond_init_4[0,1,0,0] = samples[4]
+        cond_init_4[1,0,0,0] = samples[4]
+
+        cond_init_4[2,3,0,0] = samples[5]
+        cond_init_4[3,2,0,0] = samples[5]
+
+        # Vertical 
+        cond_init_4[0,2,0,0] = samples[6]
+        cond_init_4[2,0,0,0] = samples[6]
+
+        cond_init_4[1,3,0,0] = samples[7]
+        cond_init_4[3,1,0,0] = samples[7]
+        
+        print(cond_init_4[:,:,0,0])
+
     return cond_init_4
