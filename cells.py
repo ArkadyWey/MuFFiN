7142f454af910e492168a34275ba06facdae94d8
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-07-07 13:11:04 +0100

    Add random structure initialisation
    - This commit implements code for building correct Delaunay triangulation
    in a unit cell so that
    cell can be initialised with a random
    structure and conductance
    inversely proportional to edge length.
    
    run_cell_random-structure.py:
    - First implementation of triangulation method that supervisors approve of.
    - Method outline is... get the coordinates of node in unit cell,
    use these to make coordinates
    in 9 cells,
    get the triangulation in all 9 cells,
    then delete edges that dont have an end
    in unit cell,
    then on remaining edges, define conductance
    as inverse length of edge multiplied by
    the mean we see in the random conductance case.
    This method is identical to that used by Jon in Wilmott paper.
    - Important note! -- The length of an everage edge will
    decrease by root(N) as we increase
    N in perm-dist experiments... This will increase conductance by root(N).
    To make fair test, expect to have to divide results by root(N).
    
    plot_cell_random-structure.py
    - Plot the triangulation in all 9 cells.
    - Plot the resulting initial conductance
    edges in the unit cell.  Make linethickness
    proportional to inverse length so
    that line thickness represents conductance.
    
    cells.py
    - Implement a class that executes methods in run_cell_random-structure.
    - This automates the run_cell_random-structure and spits out an
    initial conductance
    tensor at the end.
    
    initial_conditions_2D.py
    - Use the random-structured cell class above to implement a randomly structured
    initial conductance in the initial
    conditions module.
    This is necesrary as the run file uses inital conditions module.

--- cells.py
+++ cells.py
@@ -1,5 +1,6 @@
 import numpy
 import scipy
+from scipy import spatial
 
 
 
@@ -63,3 +64,294 @@ class Cell_2D_Grid:
 
 
   
+
+class Cell_2D_Random_Structure():
+    """
+    """ 
+    def __init__(self, num_nodes: int,
+                       num_refs: int, 
+                       num_dims: int):
+        """
+        """
+        # Parameters
+        self.num_nodes = num_nodes
+        self.num_refs  = num_refs
+        self.num_dims  = num_dims
+
+        (self.pts_x_0, self.pts_y_0, self.pts_x_1, self.pts_y_1, self.pts_x_m1, self.pts_y_m1) = self.get_node_coordinates()
+
+        self.pts_4 = self.get_points_tensor()
+
+        self.dist_6 = self.get_distance_between_points()
+
+        (self.simplices, self.key) = self.get_simplices_of_triangulation()
+
+        self.edges = self.get_edges()
+
+        self.cond_init_4 = self.get_conductance()
+
+
+    def get_node_coordinates(self):
+        """
+        Get x and y coordinates of each point.
+        
+        Returns
+        -------
+        - pts_x_0: numpy.ndarray
+            pts_x_0[i] is x coordinate of node i in cell with r = 0.
+        - pts_y_0: numpy.ndarray
+            pts_y_0[i] is y coordinate of node i in cell with s = 0.
+        - pts_x_1: numpy.ndarray
+            pts_x_1[i] is x coordinate of node i in cell with r = 1.
+        - pts_y_1: numpy.ndarray
+            pts_y_1[i] is y coordinate of node i in cell with s = 1.
+        - pts_x_m1: numpy.ndarray
+            pts_x_m1[i] is x coordinate of node i in cell with r = -1.
+        - pts_y_m1: numpy.ndarray
+            pts_y_m1[i] is y coordinate of node i in cell with s = -1.
+        """
+        # Get unit cell points
+        pts_x_0 = numpy.random.uniform(low=0.0, high=1.0, size=self.num_nodes) 
+        pts_y_0 = numpy.random.uniform(low=0.0, high=1.0, size=self.num_nodes) 
+
+        # Right and up components
+        pts_x_1 = 1.0*numpy.ones_like(pts_x_0) + pts_x_0 
+        pts_y_1 = 1.0*numpy.ones_like(pts_y_0) + pts_y_0
+
+        ## Left and down components
+        pts_x_m1 = -1.0*numpy.ones_like(pts_x_0) + pts_x_0
+        pts_y_m1 = -1.0*numpy.ones_like(pts_y_0) + pts_y_0
+
+        return (pts_x_0, 
+                pts_y_0,
+                pts_x_1,
+                pts_y_1,
+                pts_x_m1,
+                pts_y_m1)
+
+
+    def get_points_tensor(self):
+        """
+        Put node coordinates into tensor.
+
+        Returns
+        -------
+        - pts_4: numpy.ndarray
+            pts_4[i,m,r,s] is the x^m (either x or y) component of node i in cell at reference r,s.
+        """
+        # Parameters 
+        num_nodes = self.num_nodes 
+        num_refs  = self.num_refs
+        num_dims  = self.num_dims
+
+        pts_x_0   = self.pts_x_0
+        pts_y_0   = self.pts_y_0
+        pts_x_1   = self.pts_x_1
+        pts_y_1   = self.pts_y_1
+        pts_x_m1   = self.pts_x_m1
+        pts_y_m1   = self.pts_y_m1
+
+        # Make empty array
+        pts_4 = numpy.zeros(shape=(num_nodes,num_dims,num_refs,num_refs))
+        
+        for r in range(num_refs):
+            for s in range(num_refs):
+                for i in range(num_nodes):
+
+                    if r == 0:
+                        pts_x = pts_x_0
+                    elif r == 1:
+                        pts_x = pts_x_1
+                    elif r == 2: 
+                        pts_x = pts_x_m1
+
+                    if s == 0:
+                        pts_y = pts_y_0
+                    elif s == 1:
+                        pts_y = pts_y_1
+                    elif s == 2: 
+                        pts_y = pts_y_m1
+
+                    pts_4[i,0,r,s] = pts_x[i]
+                    pts_4[i,1,r,s] = pts_y[i]      
+
+        return pts_4
+
+
+    def get_distance_between_points(self):
+        """
+        For each pair of points (i,r,s) in the points tensor,
+        get their x and y coordinates and calculate the euclidean distance between them.
+
+        Returns 
+        --------
+
+        """
+        # Parameters
+        num_nodes = self.num_nodes
+        num_refs  = self.num_refs 
+
+        pts_4     = self.pts_4
+        
+        dist_6 = numpy.zeros(shape=(num_nodes,num_refs,num_refs,num_nodes,num_refs,num_refs))
+        # dist_6[i,r_i,s_i, j,r_j,s_j] = distance between node (i,r_i,s_i) and (j,r_j,s_j)
+        
+        for r_i in range(num_refs):
+            for s_i in range(num_refs):
+                for r_j in range(num_refs):
+                    for s_j in range(num_refs):
+                        for i in range(num_nodes):
+                            for j in range(num_nodes):
+                                # Get x,y coordinates corresponding to nodes
+                                p_i = pts_4[i,:,r_i,s_i]
+                                p_j = pts_4[j,:,r_j,s_j]
+                                # Get distance between points
+                                dist_6[i,r_i,s_i,j,r_j,s_j] = numpy.linalg.norm(p_i-p_j)
+
+        return dist_6
+
+
+    def get_simplices_of_triangulation(self):
+        """
+        Get all points in points tensor in correct form to use triangulation 
+        method. 
+        Then Triangulate all points using Delaunay triangulation.
+
+        Returns 
+        -------- 
+        - simplices: list of lists.
+            simplices[s] = [p_s_1,p_s_2,p_s_3] where p_s_i is the ith point on the s^th simplex.
+            I.e. each simplex is a list of three points that make a triangle.
+            These are indexed by p, hence we need a key between p and our indexing system (i,r,s).
+        - key: list
+            key[p] = [i,r,s] triple corresponding to point p. That is, a triple 
+            that tells us the index, and r,s cell position of the point p. 
+            We use this to map between two indexings, p, and (i,r,s).
+        """
+        # Parameters 
+        num_refs  = self.num_refs
+        num_nodes = self.num_nodes
+
+        pts_4     = self.pts_4
+
+        # Make empty lists
+        # -----
+        pts_to_tri_2 = []
+        key = []
+        # pts_to_tri_2[p,m] = mth (x or y) component of point p - need in this form for Delaunay algorithm.
+        # key[p] = [i,r,s] triple corresponding to point p
+        
+        # Get all points in correct form
+        # -----
+        for r in range(num_refs):
+            for s in range(num_refs):
+                for i in range(num_nodes):
+                    i_x = pts_4[i,0,r,s] # x component of point corresponding to node i in cell r,s
+                    i_y = pts_4[i,1,r,s] # y component of point corresponding to node i in cell r,s
+
+                    pts_to_tri_2.append([i_x,i_y]) 
+                    key.append([i,r,s])
+
+        pts_to_tri_2 = numpy.array(pts_to_tri_2)
+
+        # Triangulate points
+        tri = spatial.Delaunay(points=pts_to_tri_2)
+        
+        # Get simplices
+        simplices = tri.simplices
+        # simplices[s] = [p_s_1,p_s_2,p_s_3] where p_s_i is the ith point on the s^th simplex.
+
+        return (simplices, key)
+
+
+    def get_edges(self):
+        """
+        Turn each simplex into a loop, then extract the three edges from that loop. 
+        
+        Returns
+        -------
+        - edges: list
+            Unstructured list of edges, such that edges[e] = (p_1,p_2).
+            Note that indexing is still done via p here.
+        
+        """
+        # Parameters 
+        # -----------
+        simplices = self.simplices
+
+        loops = []
+        for simplex in simplices: 
+            path = list(simplex)
+
+            # Close the path into a loop by adding the first element at the end
+            path.append(path[0])
+            loops.append(path)
+
+
+        edges = []
+        for loop in loops:
+            # Add the three edges contained in the triangular loop
+            # NB there are always three becuase it's a triangle
+            edge_1 = [loop[0], loop[1]]
+            edge_2 = [loop[1], loop[2]]
+            edge_3 = [loop[2], loop[3]]
+
+            edges.append(edge_1)
+            edges.append(edge_2)
+            edges.append(edge_3)
+
+        return edges
+
+
+    def get_conductance(self):
+        """
+        Get the initial conductance tensor.
+        For each edge: 
+            - Get the points on either end of the edge 
+            in terms of the index p. 
+            - Convert each p index to a (i,r,s) triple  using key.
+            - Check if the edge has an end inside the unit cell. 
+            - If it does, then define the edge conductance in terms of the length of the edge.
+            - If it doesn't, then do not add the edge to the conductance tensor.
+        
+        Returns
+        -------
+        - cond_init_4: numpy.ndarray
+            cond_init_4[i,j,r,s] = conductance between node i in unit cell 
+            and node j in cell at position (r,s) relative to unit cell.
+        """
+        # Parameters 
+        # ----------
+        num_nodes = self.num_nodes
+        num_refs  = self.num_refs
+
+        edges     = self.edges
+        key       = self.key
+        dist_6    = self.dist_6
+
+        cond_init_4 = numpy.zeros(shape=(num_nodes,num_nodes,num_refs,num_refs))
+
+        for edge in edges:
+            # Get points that edge involves
+            p_i = edge[0]
+            p_j = edge[1]
+
+            # Get i,r,s triples that edge involves, by using key
+            [i_i, r_i, s_i] = key[p_i]
+            [i_j, r_j, s_j] = key[p_j]
+
+            # Keep edge if involves unit cell
+            # Either i or j is in unit cell, such that r==0==s.
+            if (r_i == 0 and s_i == 0):
+                # i is in unit cell
+                cond_init_4[i_i,i_j,r_j,s_j]   = (1.72461)*1.0/dist_6[i_i,r_i,s_i,i_j,r_j,s_j] #(1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_i,r_i,s_i,i_j,r_j,s_j])
+                cond_init_4[i_j,i_i,-r_j,-s_j] = (1.72461)*1.0/dist_6[i_i,r_i,s_i,i_j,r_j,s_j] #(1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_i,r_i,s_i,i_j,r_j,s_j]) 
+            elif (r_j == 0 and s_j == 0):
+                # j is in unit cell
+                cond_init_4[i_j,i_i,r_i,s_i]   = (1.72461)*1.0/dist_6[i_i,r_i,s_i,i_j,r_j,s_j] #(1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_j,r_j,s_j,i_i,r_i,s_i])
+                cond_init_4[i_i,i_j,-r_i,-s_i] = (1.72461)*1.0/dist_6[i_i,r_i,s_i,i_j,r_j,s_j] #(1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_j,r_j,s_j,i_i,r_i,s_i])
+            else: 
+                # neither i or j in unit cell so this edge is not in conductance
+                pass
+
+        return cond_init_4
--- initial_conditions_2D.py
+++ initial_conditions_2D.py
@@ -1,6 +1,4 @@
 import numpy
-import networkx
-from scipy import spatial
 
 import cells 
 
@@ -21,10 +19,10 @@ def grid_prescribed(num_nodes: int, num_refs: int):
 
         # Grid of one node
         # ----------------
-        cond_init_4[0,0,1,0] = 1.0 #1.72461
+        cond_init_4[0,0,1,0]  = 1.0 #1.72461
         cond_init_4[0,0,-1,0] = 1.0 #1.72461
 
-        cond_init_4[0,0,0,1] = 1.0 #1.72461
+        cond_init_4[0,0,0,1]  = 1.0 #1.72461
         cond_init_4[0,0,0,-1] = 1.0 #1.72461
 
 
@@ -47,31 +45,88 @@ def grid_prescribed(num_nodes: int, num_refs: int):
         #           0         1
 
         # Internal edges
-        cond_init_4[0,1,0,0] = 2.0#0.8 #1.0
-        cond_init_4[1,0,0,0] = 2.0#0.8 #1.0
+        cond_init_4[0,1,0,0] = 1.0#0.8 #1.0
+        cond_init_4[1,0,0,0] = 1.0#0.8 #1.0
 
-        cond_init_4[1,3,0,0] = 2.0#0.2 #1.0
-        cond_init_4[3,1,0,0] = 2.0#0.2 #1.0
+        cond_init_4[1,3,0,0] = 1.0#0.2 #1.0
+        cond_init_4[3,1,0,0] = 1.0#0.2 #1.0
 
-        cond_init_4[2,3,0,0] = 2.0#0.4 #1.0
-        cond_init_4[3,2,0,0] = 2.0#0.4 #1.0
+        cond_init_4[2,3,0,0] = 1.0#0.4 #1.0
+        cond_init_4[3,2,0,0] = 1.0#0.4 #1.0
 
-        cond_init_4[0,2,0,0] = 2.0#0.6 #1.0
-        cond_init_4[2,0,0,0] = 2.0#0.6 #1.0
+        cond_init_4[0,2,0,0] = 1.0#0.6 #1.0
+        cond_init_4[2,0,0,0] = 1.0#0.6 #1.0
 
         ## External edges
-        cond_init_4[1,0,1,0]  = 2.0#1.0 #1.0
-        cond_init_4[0,1,-1,0] = 2.0#1.0 #1.0
+        cond_init_4[1,0,1,0]  = 1.0#1.0 #1.0
+        cond_init_4[0,1,-1,0] = 1.0#1.0 #1.0
 
-        cond_init_4[3,2,1,0]  = 2.0#1.0 #1.0
-        cond_init_4[2,3,-1,0] = 2.0#1.0 #1.0
-        #
-        #cond_init_4[0,2,0,1]  = 1.0
-        #cond_init_4[2,0,0,-1] = 1.0
-        #
-        #cond_init_4[1,3,0,1] = 1.0
-        #cond_init_4[3,1,0,-1] = 1.0
+        cond_init_4[3,2,1,0]  = 1.0#1.0 #1.0
+        cond_init_4[2,3,-1,0] = 1.0#1.0 #1.0
+        
+        cond_init_4[0,2,0,1]  = 1.0
+        cond_init_4[2,0,0,-1] = 1.0
+        
+        cond_init_4[1,3,0,1]  = 1.0
+        cond_init_4[3,1,0,-1] = 1.0
 
+
+    elif num_nodes == 9:
+        # Internal edges
+        cond_init_4[0,1,0,0] = 1.0#0.8 #1.0
+        cond_init_4[1,0,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[1,2,0,0] = 1.0#0.8 #1.0
+        cond_init_4[2,1,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[3,4,0,0] = 1.0#0.8 #1.0
+        cond_init_4[4,3,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[4,5,0,0] = 1.0#0.8 #1.0
+        cond_init_4[5,4,0,0] = 1.0#0.8 #1.0
+        
+        cond_init_4[6,7,0,0] = 1.0#0.8 #1.0
+        cond_init_4[7,6,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[7,8,0,0] = 1.0#0.8 #1.0
+        cond_init_4[8,7,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[0,3,0,0] = 1.0#0.8 #1.0
+        cond_init_4[3,0,0,0] = 1.0#0.8 #1.0
+        
+        cond_init_4[1,4,0,0] = 1.0#0.8 #1.0
+        cond_init_4[4,1,0,0] = 1.0#0.8 #1.0
+        
+        cond_init_4[2,5,0,0] = 1.0#0.8 #1.0
+        cond_init_4[5,2,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[3,6,0,0] = 1.0#0.8 #1.0
+        cond_init_4[6,3,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[4,7,0,0] = 1.0#0.8 #1.0
+        cond_init_4[7,4,0,0] = 1.0#0.8 #1.0
+
+        cond_init_4[5,8,0,0] = 1.0#0.8 #1.0
+        cond_init_4[8,5,0,0] = 1.0#0.8 #1.0
+
+        ## External edges
+        cond_init_4[2,0,1,0]  = 1.0#1.0 #1.0
+        cond_init_4[0,2,-1,0] = 1.0#1.0 #1.0
+
+        cond_init_4[5,3,1,0]  = 1.0#1.0 #1.0
+        cond_init_4[3,5,-1,0] = 1.0#1.0 #1.0
+
+        cond_init_4[8,6,1,0]  = 1.0#1.0 #1.0
+        cond_init_4[6,8,-1,0] = 1.0#1.0 #1.0
+
+        cond_init_4[0,6,0,1]  = 1.0#1.0 #1.0
+        cond_init_4[6,0,0,-1] = 1.0#1.0 #1.0
+
+        cond_init_4[1,7,0,1]  = 1.0#1.0 #1.0
+        cond_init_4[7,1,0,-1] = 1.0#1.0 #1.0
+
+        cond_init_4[2,8,0,1]  = 1.0#1.0 #1.0
+        cond_init_4[8,2,0,-1] = 1.0#1.0 #1.0
 
     return cond_init_4
 
@@ -198,342 +253,10 @@ def random_structure_uniform(num_nodes: int, num_refs: int):
     """
     num_dims  = 2
 
-    # Get unit cell points
-    pts_x_0 = numpy.random.uniform(low=0.0, high=1.0, size=num_nodes) #numpy.array([0.5])#
-    pts_y_0 = numpy.random.uniform(low=0.0, high=1.0, size=num_nodes) #numpy.array([0.5])#
+    cell = cells.Cell_2D_Random_Structure(num_nodes=num_nodes,
+                                          num_refs=num_refs,
+                                          num_dims=num_dims)
 
-    # Right or up components
-    pts_x_1 = 1.0*numpy.ones_like(pts_x_0) + pts_x_0 
-    pts_y_1 = 1.0*numpy.ones_like(pts_y_0) + pts_y_0
-
-    ## Left or down components
-    pts_x_m1 = -1.0*numpy.ones_like(pts_x_0) + pts_x_0
-    pts_y_m1 = -1.0*numpy.ones_like(pts_y_0) + pts_y_0
-
-
-
-    # Get points tensor 
-    # ------------------
-    pts_4 = numpy.zeros(shape=(num_nodes,num_dims,num_refs,num_refs))
-    # pts_4[i,m,r,s] is the x^m component of node i in cell at reference r,s
-
-    for r in range(num_refs):
-        for s in range(num_refs):
-            for i in range(num_nodes):
-
-                if r == 0:
-                    pts_x = pts_x_0
-                elif r == 1:
-                    pts_x = pts_x_1
-                elif r == 2: 
-                    pts_x = pts_x_m1
-
-                if s == 0:
-                    pts_y = pts_y_0
-                elif s == 1:
-                    pts_y = pts_y_1
-                elif s == 2: 
-                    pts_y = pts_y_m1
-
-                pts_4[i,0,r,s] = pts_x[i]
-                pts_4[i,1,r,s] = pts_y[i]
-
-
-
-
-    # Triangulate unit cell with upper quartile
-    # -----------------------------------------
-    # The rest will be made via reflection
-    pts_to_tri_2 = []
-    key = []
-    # pts_to_tri_2[p,m] = mth component of point p
-    # key[p] = [i,r,s] corresponding to point p
-    for r in range(2):
-        for s in range(2):
-            for i in range(num_nodes):
-                i_x = pts_4[i,0,r,s] # x component of point corresponding to node i in cell r,s
-                i_y = pts_4[i,1,r,s] # y component of point corresponding to node i in cell r,s
-
-                pts_to_tri_2.append([i_x,i_y]) 
-                key.append([i,r,s])
-
-    pts_to_tri_2 = numpy.array(pts_to_tri_2)
-
-    tri = spatial.Delaunay(points=pts_to_tri_2)
-    simplices = tri.simplices
-
-
-
-    # Get edges given by triangulation
-    # --------------------------------
-    loops = []
-    for simplex in simplices: 
-        path = list(simplex)
-        path.append(path[0])
-        loops.append(path)
-
-
-    edges = []
-    for loop in loops:
-        # Add the three edges contained in the triangular loop
-        # NB there are always three becuase it's a triangle
-        edge_1 = [loop[0], loop[1]]
-        edge_2 = [loop[1], loop[2]]
-        edge_3 = [loop[2], loop[3]]
-
-        edges.append(edge_1)
-        edges.append(edge_2)
-        edges.append(edge_3)
-
-
-    # Get distances between points
-    # ----------------------------
-    dist_6 = numpy.zeros(shape=(num_nodes,num_refs,num_refs,num_nodes,num_refs,num_refs))
-    # dist_6[i,r_i,s_i, j,r_j,s_j] = distance between node (i,r_i,s_i) and (j,r_j,s_j)
-    for r_i in range(num_refs):
-        for s_i in range(num_refs):
-            for r_j in range(num_refs):
-                for s_j in range(num_refs):
-                    for i in range(num_nodes):
-                        for j in range(num_nodes):
-                            # Get points corresponding to nodes
-                            p_i = pts_4[i,:,r_i,s_i]
-                            p_j = pts_4[j,:,r_j,s_j]
-                            # Get distance between points
-                            dist_6[i,r_i,s_i,j,r_j,s_j] = numpy.linalg.norm(p_i-p_j)
-
-
-
-    # Get conductance tensor 
-    # ---------------------
-    cond_init_4 = numpy.zeros(shape=(num_nodes,num_nodes,num_refs,num_refs))
-    for edge in edges:
-        # Get points that edge involves
-        p_i = edge[0]
-        p_j = edge[1]
-
-        # Get nodes that edge involves
-        n_i = key[p_i]
-        n_j = key[p_j]
-
-        # Get i,r,s triples that edge involves
-        [i_i, r_i, s_i] = n_i
-        [i_j, r_j, s_j] = n_j
-
-        # Keep edge if involves unit cell
-        # Either first or second node is in unit cell or they both are
-        if (r_i == 0 and s_i == 0):
-            # i is in unit cell
-            cond_init_4[i_i,i_j,r_j,s_j] = (1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_i,r_i,s_i,i_j,r_j,s_j])    #1.0
-            cond_init_4[i_j,i_i,-r_j,-s_j] = (1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_i,r_i,s_i,i_j,r_j,s_j]) #1.0
-        elif (r_j == 0 and s_j == 0):
-            # j is in unit cell
-            cond_init_4[i_j,i_i,r_i,s_i] = (1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_j,r_j,s_j,i_i,r_i,s_i])
-            cond_init_4[i_i,i_j,-r_i,-s_i] = (1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_6[i_j,r_j,s_j,i_i,r_i,s_i])
-        else: 
-            # neither i or j in unit cell so this edge is not in conductance
-            pass
-
+    cond_init_4 = cell.cond_init_4
 
     return cond_init_4
-
-#    # Define parameters:
-#    num_dims = 2
-#
-#    cond_init_4 = numpy.zeros(shape=(num_nodes, num_nodes, num_refs, num_refs))
-#
-#
-#
-#    # Get positions of all points 
-#    # ----------------------------
-#
-#    # Central components
-#    #pts_x_0 = numpy.array([0.85983879])
-#    #pts_y_0 = numpy.array([0.65102802])
-#    #pts_x_0 = numpy.array([0.5])#numpy.array([0.2,0.8,0.2,0.8])
-#    #pts_y_0 = numpy.array([0.5])#numpy.array([0.2,0.2,0.8,0.8])    
-#    pts_x_0 = numpy.random.uniform(low=0.0, high=1.0, size=num_nodes)
-#    pts_y_0 = numpy.random.uniform(low=0.0, high=1.0, size=num_nodes)
-#
-#    # Right or up components
-#    pts_x_1 = 1.0*numpy.ones_like(pts_x_0) + pts_x_0 
-#    pts_y_1 = 1.0*numpy.ones_like(pts_y_0) + pts_y_0
-#
-#    # Left or down components
-#    #pts_x_m1 = -1.0*numpy.array([el for el in reversed(list(pts_x_0))])
-#    pts_x_m1 = -1.0*numpy.ones_like(pts_x_0) + pts_x_0
-#    pts_y_m1 = -1.0*numpy.ones_like(pts_y_0) + pts_y_0
-#
-#    # Fill the positions tensor
-#    pts_4 = numpy.zeros(shape = (num_nodes, num_dims, num_refs, num_refs) )
-#    #pts_4[i,m,r,s] = num_dims[m] component of position of node nodes[i] in cell with reference (r,s)
-#
-#    for r in range(num_refs):
-#        for s in range(num_refs):
-#            if r == 0: 
-#                pts_x = pts_x_0
-#            elif r == 1: 
-#                pts_x = pts_x_1
-#            elif r == 2: 
-#                pts_x = pts_x_m1
-#
-#            if s == 0: 
-#                pts_y = pts_y_0
-#            elif s == 1: 
-#                pts_y = pts_y_1
-#            elif s == 2: 
-#                pts_y = pts_y_m1
-#
-#            pts_2 = numpy.transpose(numpy.concatenate(([pts_x],[pts_y]), axis=0))
-#
-#            pts_4[:,:,r,s] = pts_2[:,:]
-#
-#
-#
-#
-#
-#
-#
-#
-#    # Triangulation
-#    # --------------
-#
-#    # Transform points into correct format for triangulation
-#    # ----------------------------------------------------
-#    points  = []
-#    key = []
-#    # key[p] = [i,r,s]. So the pth entry of the adjacency matrix 
-#    # orresponds to the (i,r,s) node. 
-#    # This provides a mapping between teh graph and the indexing for cond.
-#
-#    for r in range(num_refs):
-#        for s in range(num_refs):
-#            for i in range(num_nodes):
-#                points.append([pts_4[i,0,r,s],pts_4[i,1,r,s]])
-#                key.append(numpy.array([i,r,s]))
-#
-#    # Triangulation requires array
-#    points = numpy.array(points)
-#    # points[p,m] = mth component of pth point
-#
-#
-#    # Carry out Delauney triangulation 
-#    # ------------------------------
-#    tri = spatial.Delaunay(points=points)
-#    #from pyhull.delaunay import DelaunayTri
-#    #tri = DelaunayTri(points=points)
-#    #print(tri.simplices)
-#
-#
-#
-#
-#
-#    # Get adjacency matrix of all nine cells
-#    # --------------------------------------
-#    simplices = tri.simplices
-#    # NB: Simplices are sets of three points 
-#    # thta make triangles.
-#
-#
-#    # 1. Get closed cycle from simplex
-#    # NB This is set of four points 
-#    # to close the triangle. 
-#    # Makes getting edges easily.
-#    loops = []
-#
-#    for simplex in simplices: 
-#        path = list(simplex)
-#        path.append(path[0])
-#        loops.append(path)
-#
-#
-#
-#
-#    # 2. Get list of all edge tuples
-#    edges = []
-#
-#    for loop in loops:
-#        # Add the two edges contained in the triangular loop
-#        # NB there are always  two becuase it's a triangle
-#        edge_1 = [loop[0],loop[1]]
-#        edge_2 = [loop[2], loop[3]]
-#
-#        edges.append(edge_1)
-#        edges.append(edge_2)
-#
-#        # Add the corresponding edges since we'll need a symmetric adj matrix
-#        edge_1_reversed = [loop[1],loop[0]]
-#        edge_2_reversed = [loop[3], loop[2]]
-#
-#        edges.append(edge_1_reversed)
-#        edges.append(edge_2_reversed)
-#
-#
-#
-#    # 3. Get adj from list of edges 
-#    num_pts = num_nodes*9
-#    A = numpy.zeros(shape=(num_pts,num_pts))   
-#
-#    for edge in edges:
-#        pi = edge[0]
-#        pj = edge[1]
-#
-#        A[pi,pj] = 1
-#
-#
-#    # Put actual weights into this adjacency matrix 
-#    # -----------------------------------
-#
-#    # Get weights between points
-#    dist_2 = numpy.zeros_like(A)
-#    weig_2 = numpy.zeros_like(A)
-#    # weigh_2[pi,pj] = weight between point pi and pj
-#    for i in range(num_pts):
-#        pi = numpy.array(points[i,:]) 
-#        for j in range(num_pts):
-#            pj = numpy.array(points[j,:])
-#
-#            if i!=j:
-#                dist_2[i,j] = numpy.linalg.norm(pi-pj)
-#                weig_2[i,j] = (1.72461)*(1/numpy.sqrt(num_nodes))*(1/dist_2[i,j])
-#
-#
-#
-#    # Get weighted adjacency matrix
-#    A = A*weig_2
-#
-#
-#
-#    # Get the cond from this adjacency matrix
-#    # --------------------------------
-#    for i in range(num_nodes):
-#        for j in range(num_nodes):
-#            for r in range(num_refs):
-#                for s in range(num_refs):
-#                    if r ==0:
-#                        mr = 0
-#                    elif r == 1:
-#                        mr = 2
-#                    elif r == 2:
-#                        mr = 1
-#
-#                    if s == 0:
-#                        ms = 0
-#                    elif s == 1:
-#                        ms = 2
-#                    elif s == 2:
-#                        ms = 1
-#                    
-#                    #if r!=0 and s!=0:
-#                    #    pass
-#                    #else:
-#                    #    # Get p corresponding to j,r,s
-#                    for p in range(num_pts):
-#                        if numpy.array_equal(a1=key[p], a2=numpy.array([j,r,s])):
-#                            # Fill edge (i,j,r,s) where i is in reference cell
-#                            cond_init_4[i,j,r,s] = A[i,p]
-#                            cond_init_4[j,i,mr,ms] = cond_init_4[i,j,r,s]
-#
-#
-#
-#    return cond_init_4
