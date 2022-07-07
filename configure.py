b94a16a473ad41764c64c8e165b56d9554fb9fb1
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-07-07 16:10:29 +0100

    Add initialisation parameter
    - We can now run 2D preprocessing and
     flow with four different initialisations...
    -square-struc_prescribed -- this is asquare
    lattice with a prescried conductacne on each edge.
    - square-struc -- this is square lattice with edges
    drawn from lognormal distribution.
    - hexag-struc -- this is hexagonal lattice with edges drawn from
    log-normal distribution
    - rand-struc -- this is random structure with edge conductances
    proportional to their length.
    
    - To avoid getting wrong initialisation,
    add parameter whihc chooses a different initial condition
    depending on which string is given... This avoids previous
    problem that one would specify parameter dictionary but also
    have to manually edit the config file...
    Now give initial condition as a parameter and
    this automatically alters the config file to give correct initial
    condition.

--- configure.py
+++ configure.py
@@ -23,16 +23,17 @@ class Configure():
         file = open("parameters.json", "r")
         parameters = json.load(file)
         
-        self.max_ref_dist = parameters["max_ref_dist"]
-        self.num_dims     = parameters["num_dims"]
-        self.num_concs    = parameters["num_concs"]
-        self.alpha        = parameters["alpha"]
-        self.v            = parameters["v"]    # 2.0 # Sum of volumes of nodes in cell
-        self.phi          = parameters["phi"]  # TODO: Define this properly
-        self.l1           = parameters["l1"]
-        self.l2           = parameters["l2"]
-        self.mean         = parameters["mean"] 
-        self.sd           = parameters["sd"]
+        self.max_ref_dist   = parameters["max_ref_dist"]
+        self.num_dims       = parameters["num_dims"]
+        self.num_concs      = parameters["num_concs"]
+        self.alpha          = parameters["alpha"]
+        self.v              = parameters["v"]    # 2.0 # Sum of volumes of nodes in cell
+        self.phi            = parameters["phi"]  # TODO: Define this properly
+        self.l1             = parameters["l1"]
+        self.l2             = parameters["l2"]
+        self.mean           = parameters["mean"] 
+        self.sd             = parameters["sd"]
+        self.initialisation = parameters["initialisation"]
 
 
         # Do secondary configuration 
@@ -50,19 +51,37 @@ class Configure():
 
         self.adhe_init_4 = numpy.zeros(shape=(self.num_nodes, self.num_nodes, self.num_refs, self.num_refs)) 
 
-        #self.cond_init_4 = initial_conditions_2D.grid_prescribed(num_nodes=self.num_nodes,
-        #                                                         num_refs=self.num_refs)
+        self.cond_init_4 = self.get_initial_conductance()
 
-        #self.cond_init_4 = initial_conditions_2D.grid_log_normal(num_nodes=self.num_nodes, 
-        #                                                         num_refs=self.num_refs, 
-        #                                                         mean=self.mean,
-        #                                                         sd=self.sd)
 
-        self.cond_init_4 = initial_conditions_2D.random_structure_uniform(num_nodes=self.num_nodes,
-                                                                          num_refs=self.num_refs)
+    def get_initial_conductance(self):
+        """
+        The initial conductance depends on the parameter 
+        initialisation, which is prescribed in the parameters 
+        dictionary.
+        """
+        if self.initialisation == "square-struc_prescribed":
+            cond_init_4 = initial_conditions_2D.grid_prescribed(num_nodes=self.num_nodes,
+                                                                     num_refs=self.num_refs)
+        elif self.initialisation == "square-struc":
+            cond_init_4 = initial_conditions_2D.grid_log_normal(num_nodes=self.num_nodes, 
+                                                                     num_refs=self.num_refs, 
+                                                                     mean=self.mean,
+                                                                     sd=self.sd)
+        elif self.initialisation == "rand-struc":
+            cond_init_4 = initial_conditions_2D.random_structure_uniform(num_nodes=self.num_nodes,
+                                                                              num_refs=self.num_refs)
 
-    
+        elif self.initialisation == "hexag-struc":
+            raise Exception("Need to implement hexag-struc...")
 
+        else: 
+            raise Exception("""initialisation must be: square-struc_prescribed or \
+                               square-struc or rand-struc or hexag-struc.""")
+
+        return cond_init_4
+
+        
 if __name__ == "__main__":
 
     num_nodes = 1
