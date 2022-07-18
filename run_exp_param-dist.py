5e7275e9d4f7a20a95acb5560df53ec4c5aea8e6
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-07-18 22:21:23 +0100

    Prep for 6ireg run_exp_param-dist

--- run_exp_param-dist.py
+++ run_exp_param-dist.py
@@ -30,7 +30,7 @@ num_nodes_list = args.num_nodes_list
 num_reps = args.num_reps#100 # number of times to repeat a test
 
 
-path_results = os.path.join(".","results/results_exp_param-dist_4-reg_reps-{}".format(num_reps))
+path_results = os.path.join(".","results/results_exp_param-dist_6-ireg_reps-{}".format(num_reps))
 if not os.path.exists(path_results):
     os.makedirs(path_results)
 
