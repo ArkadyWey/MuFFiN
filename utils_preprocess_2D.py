936ada901c74f74d4c44efb03aa137e262ea22cb
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-07-26 16:01:29 +0100

    Add horizontal and vetical edge count
    - Found that j is dictated by number of
    edges parallel to flow that are blocked.
    - So add coutning these to blocked edge
    counting function.
    - In 4-reg case, horizontal edges are on the block leading diagonal of conductance,
    and are thus easy to isolate.
    - Note that in 6-reg and
    6-reg structures, the important quanrtity may not be horzontal edges, since there's no
    such concept in these structures.

--- utils_preprocess_2D.py
+++ utils_preprocess_2D.py
@@ -1,39 +1,47 @@
 import numpy 
-import copy
 
-def count_num_edges_blocked(adhe_tabl_5, heav_5, delt_5, cond_init_4):
+def count_num_edges_blocked(cond_tabl_5, adhe_tabl_5, delt_5, heav_5):
     """
-    Count the number of edges that are blocked 
-    in particular run.
+    Count the number of edges that are blocked in particular run.
+    Also count the number of these that are horizontal and vertical. 
+    I think the latter part of this method only works for 4-reg structure.
+
+    
     """
     # Parameters 
-    m = 0
-    num_refs    = len(adhe_tabl_5[-1,0,0,:,0])
-
-    adhe_tabl_4 = adhe_tabl_5[-1,:,:,:,:]
-    # adhe_tabl_4[i,j,r,s]
-
-    heav_4      = heav_5[0,:,:,:,:]
-    
-    delt_4 = delt_5[0,:,:,:,:]
-    delt_ind_4 = copy.copy(delt_4)
-    delt_ind_4[delt_ind_4!=0]
-    
-    # Indicators
-    cond_init_ind_4 = copy.copy(cond_init_4)
-    cond_init_ind_4[cond_init_ind_4!=0.0]
+    # -----------
+    m         = 0 # interested in flow in horizontal direction
+    num_refs  = len(cond_tabl_5[0,0,0,:,0])
+    num_nodes = len(cond_tabl_5[0,:,0,0,0])
+    n         = int(numpy.sqrt(num_nodes)) # num_rows_or_cols
 
 
-    
     count_adhe = 0
+    count_adhe_hori = 0
     for r in range(num_refs):
         for s in range(num_refs):
-
-            ## Take upper triangle so that edges are unique
-            #a = adhe_wo_reps_2 = numpy.triu(adhe_tabl_4[:,:,r,s])
-
-            a = cond_init_4[:,:,r,s]*adhe_tabl_4[:,:,r,s]*heav_4[:,:,r,m]*delt_ind_4[:,:,r,m] #*cond_init_ind_4[:,:,r,s]
+            
+            # All edges
+            # ---------
+            # Get integrand of adhesivity
+            a = cond_tabl_5[0,:,:,r,s]*(-delt_5[0,:,:,r,m])*adhe_tabl_5[-1,:,:,r,s]*heav_5[0,:,:,r,m]
+            
             # Count number of unique edges where adhesivity is 1
             count_adhe = count_adhe + numpy.count_nonzero(a=a, axis=None, keepdims=False)
-    
-    return count_adhe
+
+            # Horizontal edges 
+            # -----------------
+            # Count number oghorizontal edges
+            if s == 0: # Only hozizontal if s==0
+                for b in range(n):
+                    # Get integrand of adhesivity
+                    a_hori = cond_tabl_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,s]*(-delt_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,m])*adhe_tabl_5[-1,b*n:(b+1)*n,b*n:(b+1)*n,r,s]*heav_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,m]
+                    
+                    # Count number of horizontal edges where adhesivity is 1
+                    # This is number of non zeros in block diagonals
+                    count_adhe_hori = count_adhe_hori + numpy.count_nonzero(a=a_hori, axis=None, keepdims=False)
+
+    # Count vertical edges 
+    count_adhe_vert = count_adhe-count_adhe_hori
+
+    return count_adhe, count_adhe_hori, count_adhe_vert
