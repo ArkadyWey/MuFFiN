93816fac1cb921c534dd41df2460f3cefa18749e
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-08-08 00:24:10 +0100

    Add blocked horizontal edge counting for 6-reg
    - Before, we only had an algorithm to count the
    types of edges that are blocked in the 4-reg case.
    - Here we extend this algorithm to indiciate whether thee
    edges that are blocked are horizontal edges,
    since this is important in predicting the structure
    of j.

--- utils_preprocess_2D.py
+++ utils_preprocess_2D.py
@@ -1,6 +1,6 @@
 import numpy 
 
-def count_num_edges_blocked(cond_tabl_5, adhe_tabl_5, delt_5, heav_5):
+def count_num_edges_blocked(initialisation, cond_tabl_5, adhe_tabl_5, delt_5, heav_5):
     """
     Count the number of edges that are blocked in particular run.
     Also count the number of these that are horizontal and vertical. 
@@ -29,19 +29,144 @@ def count_num_edges_blocked(cond_tabl_5, adhe_tabl_5, delt_5, heav_5):
             # Count number of unique edges where adhesivity is 1
             count_adhe = count_adhe + numpy.count_nonzero(a=a, axis=None, keepdims=False)
 
-            # Horizontal edges 
-            # -----------------
-            # Count number oghorizontal edges
-            if s == 0: # Only hozizontal if s==0
-                for b in range(n):
-                    # Get integrand of adhesivity
-                    a_hori = cond_tabl_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,s]*(-delt_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,m])*adhe_tabl_5[-1,b*n:(b+1)*n,b*n:(b+1)*n,r,s]*heav_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,m]
-                    
-                    # Count number of horizontal edges where adhesivity is 1
-                    # This is number of non zeros in block diagonals
-                    count_adhe_hori = count_adhe_hori + numpy.count_nonzero(a=a_hori, axis=None, keepdims=False)
+            #print("r={},s={},a=\n{}".format(r,s,a))
 
-    # Count vertical edges 
-    count_adhe_vert = count_adhe-count_adhe_hori
+            if initialisation=="4-reg":
+                """
+                If initialisation=="4-reg" then get the number of horizontal edges that are blocked.
+                """
+                # NOTE: Below is only valid  fpr 4-reg:
+                # Horizontal edges 
+                # -----------------
+                # Count number of horizontal edges
+                if s == 0: # Only hozizontal if s==0
+                    for b in range(n):
+                        # Get integrand of adhesivity
+                        a_hori = cond_tabl_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,s]*(-delt_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,m])*adhe_tabl_5[-1,b*n:(b+1)*n,b*n:(b+1)*n,r,s]*heav_5[0,b*n:(b+1)*n,b*n:(b+1)*n,r,m]
 
-    return count_adhe, count_adhe_hori, count_adhe_vert
+                        # Count number of horizontal edges where adhesivity is 1
+                        # This is number of non zeros in block diagonals
+                        count_adhe_hori = count_adhe_hori + numpy.count_nonzero(a=a_hori, axis=None, keepdims=False)
+
+            elif initialisation == "6-reg":
+                """
+                If initialisation=="6-reg, then check if how many horizontal edges are blocked. 
+                To do this, get the list of pairs of indices that we know will 
+                be between horizontal edges. 
+                Then for each horizontal edge pair, check whether this is blocked 
+                by checking whether there is a non-zero entry in a at this index pair.
+                """
+                horizontal_edge_pairs = get_horizontal_index_pairs(num_nodes=num_nodes)
+                for pair in horizontal_edge_pairs:
+                    left_index = pair[0]
+                    right_index = pair[1]
+                    if a[left_index,right_index] != 0.0:
+                        count_adhe_hori = count_adhe_hori + 1
+                    else: 
+                        pass
+
+            elif initialisation == "6-ireg":
+                pass 
+            
+            else: 
+                raise Exception("initilisation must be 4-reg, 6-reg, or 6-ireg.")
+
+
+    if initialisation=="4-reg":
+        """
+        If initialisation=="4-reg" then get the number of vertical edges that are blocked.
+        Use horizontal count to save computational time.
+        """
+        # Count vertical edges 
+        count_adhe_not_hori = count_adhe-count_adhe_hori
+    
+    if initialisation=="6-reg":
+        """
+        If initialisation=="6-reg" then get the number of diagonal edges that are blocked. 
+        This is the total number of edges that are blocked minus the number of horizontal edges 
+        that are blocked.
+        """
+        # Count diagonal edges 
+        count_adhe_not_hori = count_adhe-count_adhe_hori
+    elif initialisation=="6-ireg":
+        pass 
+    else:
+        raise Exception("Initialisation must be 4-reg,6-reg, or 6-ireg.")
+
+    return count_adhe, count_adhe_hori, count_adhe_not_hori
+
+
+
+def get_horizontal_index_pairs(num_nodes):
+    """
+    Get a list of all pairs of node indices that are either end 
+    of a horizontal edge in a 6-reg structure with a cell containing num_nodes
+    nodes.
+    Each pair is itself a list.
+
+    """
+    N = num_nodes
+    n = int(numpy.sqrt(N/2))
+
+    if num_nodes == 2: 
+        """
+        This is a special case, but luckily 
+        pairs are easy to identify, since there 
+        are only two.
+        """
+        pairs = [[0,0],[1,1]]
+
+    else: 
+        """
+        For every other case there is an algorithm. 
+        See notes from 7.8.22 for details.
+        """
+
+        # Define index list 
+        # -------
+        """
+        index_list is a list of constructor indices that make up a constructor 
+        set from which all index pairs are derived. 
+        This should be the set [0,1,2,...,n-1,0]
+        The zero on the end is there because we need a cycle.
+        """
+        index_list = list(numpy.linspace(start=0,stop=n,num=n,dtype=int,endpoint=False)) # use argument unpacker
+        index_list.append(0)
+
+        """
+        Given the set of constructor indices above, 
+        the below algorithm defines all possible pairs.
+        """
+        pairs = []
+        for k in range(n): # [0,...,n-1]
+            for i in range(n): # [0,...,n-1]
+
+                index_left = index_list[i]
+                index_right = index_list[i+1]
+
+                # Even pair
+                # ------
+                even_left_node_index  = k*2*n + 2*index_left
+                even_right_node_index = k*2*n + 2*index_right
+
+                even_pair = [even_left_node_index, even_right_node_index]
+
+
+                pairs.append(even_pair)
+
+
+                # Odd pair
+                # -----
+                odd_left_node_index  = k*2*n + 2*index_left + 1
+                odd_right_node_index = k*2*n + 2*index_right + 1
+
+                odd_pair = [odd_left_node_index, odd_right_node_index]
+
+                #print("odd_pair:{}".format(odd_pair))
+
+                pairs.append(odd_pair)
+
+    return pairs
+        
+
+
