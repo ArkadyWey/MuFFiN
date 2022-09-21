8390053caa4f836eed700250e04794dbe15adb5a
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-09-21 17:19:12 +0100

    Plot k,j and interpolated k,j
    - Visualise the interpolation that's taking place inside the
    solver.

--- plot_preprocess_1D.py
+++ plot_preprocess_1D.py
@@ -9,7 +9,7 @@ path_results = os.path.join(".","results/results_preprocess_1D")
 
 # Load variables
 # -----
-conc_max_discs_1 = numpy.load(os.path.join(path_results, "conc_max_discs_1.npy"))
+conc_max_disc_1 = numpy.load(os.path.join(path_results, "conc_max_disc_1.npy"))
 perm_prep_1 = numpy.load(os.path.join(path_results, "perm_prep_1.npy"))
 depo_prep_1 = numpy.load(os.path.join(path_results, "depo_prep_1.npy"))
 
@@ -18,10 +18,10 @@ depo_prep_1 = numpy.load(os.path.join(path_results, "depo_prep_1.npy"))
 # -----
 fig, ax = plt.subplots(1,1)
 
-ax.plot(conc_max_discs_1, perm_prep_1, label=r"$k^{00}$", color="tab:red")
-ax.plot(conc_max_discs_1,-depo_prep_1, label=r"$j^{0}$", color="tab:blue")
+ax.plot(conc_max_disc_1, perm_prep_1, label=r"$k^{00}$", color="tab:red")
+ax.plot(conc_max_disc_1,-depo_prep_1, label=r"$j^{0}$", color="tab:blue")
 ax.set_xlabel(r"$C_{\mathrm{max}}$")
 ax.legend()
-plt.savefig(fname=os.path.join(path_results,"perm_prep_1__depo_prep_1__v__conc_max_discs_1.svg"), format="svg")
+plt.savefig(fname=os.path.join(path_results,"perm_prep_1__depo_prep_1__v__conc_max_disc_1.svg"), format="svg")
 
 
