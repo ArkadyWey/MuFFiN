f88175feaa8a4537641399787a8297fc1839dcdf
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2022-09-21 17:13:44 +0100

    Rewrite plot_flow module
    - Include new plotting format.

--- plot_flow.py
+++ plot_flow.py
@@ -2,9 +2,13 @@ from matplotlib import pyplot as plt
 import os
 import numpy
 
+import sys
+sys.path.append("/home/user/utils_python")
+import plotting
+
 # Parameters 
 # -----
-path_results = os.path.join(".","results/results_flow_1D")
+path_results = os.path.join(".","results/results_flow")
 
 
 # Load variables 
@@ -12,12 +16,13 @@ path_results = os.path.join(".","results/results_flow_1D")
 time_1 = numpy.load(os.path.join(path_results, "time_1.npy"))
 posi_1 = numpy.load(os.path.join(path_results, "posi_1.npy"))
 
-conc_2 = numpy.load(os.path.join(path_results, "conc_2.npy"))
-velo_1 = numpy.load(os.path.join(path_results, "velo_1.npy"))
-psi_2  = numpy.load(os.path.join(path_results, "psi_2.npy"))
-perm_2 = numpy.load(os.path.join(path_results, "perm_2.npy"))
-depo_2 = numpy.load(os.path.join(path_results, "depo_2.npy"))
-dpdx_2 = numpy.load(os.path.join(path_results, "dpdx_2.npy"))
+conc_2     = numpy.load(os.path.join(path_results, "conc_2.npy"))
+conc_max_2 = numpy.load(os.path.join(path_results, "conc_max_2.npy"))
+perm_2     = numpy.load(os.path.join(path_results, "perm_2.npy"))
+depo_2     = numpy.load(os.path.join(path_results, "depo_2.npy"))
+velo_1     = numpy.load(os.path.join(path_results, "velo_1.npy"))
+dpdx_2     = numpy.load(os.path.join(path_results, "dpdx_2.npy"))
+psi_2      = numpy.load(os.path.join(path_results, "psi_2.npy"))
 
 
 num_times = len(time_1)
@@ -29,9 +34,11 @@ third_quarter  = int(3*(num_times-1)/4)
 end            = -1
 
 
-# Plot conccentration
+# Plot concentration
 # -----
+plotting.thesisify_pre_ax_creation()
 fig, ax = plt.subplots(1,1)
+
 ax.plot(posi_1,conc_2[:,start], label=r"$t=0$")
 ax.plot(posi_1,conc_2[:,first_quarter], label=r"$t=0.25$")
 ax.plot(posi_1,conc_2[:,second_quarter], label=r"$t=0.50$")
@@ -42,24 +49,39 @@ ax.set_xlabel(r"$x$")
 ax.set_ylabel(r"$c$")
 
 #ax.legend()
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$x$",
+                             y_label=r"$c$",
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
 
-plt.savefig(fname=os.path.join(path_results,"conc_2__v__posi_1.svg"), format="svg")
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"conc_2__v__posi_1.svg"), format="svg")
 
 
 # Plot velocity 
 # -----
+plotting.thesisify_pre_ax_creation()
 fig, ax = plt.subplots(1,1)
 
 ax.plot(time_1,velo_1)
 
-ax.set_xlabel(r"$t$")
-ax.set_ylabel(r"$u$")
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$t$",
+                             y_label=r"$u$",
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
 
-plt.savefig(fname=os.path.join(path_results,"velo_1__v__time_1.svg"), format="svg")
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"velo_1__v__time_1.svg"), format="svg")
+
 
 
 # Plot reaction parameter 
 # -----
+plotting.thesisify_pre_ax_creation()
 fig, ax = plt.subplots(1,1)
 
 ax.plot(posi_1,psi_2[:,start])
@@ -68,30 +90,44 @@ ax.plot(posi_1,psi_2[:,second_quarter])
 ax.plot(posi_1,psi_2[:,third_quarter])
 ax.plot(posi_1,psi_2[:,end])
 
-ax.set_xlabel(r"$x$")
-ax.set_ylabel(r"$ψ$")
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$x$",
+                             y_label=r"$\psi$",
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
 
-plt.savefig(fname=os.path.join(path_results,"psi_2__v__posi_1.svg"), format="svg")
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"psi_2__v__posi_1.svg"), format="svg")
+
 
 
 # Plot permeability 
 # -----
+plotting.thesisify_pre_ax_creation()
 fig, ax = plt.subplots(1,1)
 
 ax.plot(posi_1,perm_2[:,start])
 ax.plot(posi_1,perm_2[:,first_quarter])
-ax.plot(posi_1,perm_2[:,second_quarter])
+ax.plot(posi_1,perm_2[:,second_quarter]) #301 is index where prob starts
 ax.plot(posi_1,perm_2[:,third_quarter])
 ax.plot(posi_1,perm_2[:,end])
 
-ax.set_xlabel(r"$x$")
-ax.set_ylabel(r"$k$")
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$x$",
+                             y_label=r"$k$",
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
 
-plt.savefig(fname=os.path.join(path_results,"perm_2__v__posi_1.svg"), format="svg")
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"perm_2__v__posi_1.svg"), format="svg")
+
 
 
 # Plot deposition parameter 
 # -----
+plotting.thesisify_pre_ax_creation()
 fig, ax = plt.subplots(1,1)
 
 ax.plot(posi_1, depo_2[:,start])
@@ -100,14 +136,21 @@ ax.plot(posi_1, depo_2[:,second_quarter])
 ax.plot(posi_1, depo_2[:,third_quarter])
 ax.plot(posi_1, depo_2[:,end])
 
-ax.set_xlabel(r"$x$")
-ax.set_ylabel(r"$j$")
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$x$",
+                             y_label=r"$j$",
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
 
-plt.savefig(fname=os.path.join(path_results,"depo_2__v__posi_1.svg"), format="svg")
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"depo_2__v__posi_1.svg"), format="svg")
+
 
 
 # Plot pressure gradient 
 # -----
+plotting.thesisify_pre_ax_creation()
 fig, ax = plt.subplots(1,1)
 
 ax.plot(posi_1, dpdx_2[:,start])
@@ -116,17 +159,20 @@ ax.plot(posi_1, dpdx_2[:,second_quarter])
 ax.plot(posi_1, dpdx_2[:,third_quarter])
 ax.plot(posi_1, dpdx_2[:,end])
 
-ax.set_xlabel(r"$x$")
-ax.set_ylabel(r"$dp/dx$")
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$x$",
+                             y_label=r"$dp/dx$",
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
 
-plt.savefig(fname=os.path.join(path_results,"dpdx_2__v__posi_1.svg"), format="svg")
-
-
-
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"dpdx_2__v__posi_1.svg"), format="svg")
 
 
 # Plot permeability ad deposition on on graph
 # -----
+plotting.thesisify_pre_ax_creation()
 fig, ax = plt.subplots(1,1)
 
 ax.plot(posi_1,perm_2[:,start], label=r"$k^{00}$", color="tab:red")
@@ -135,7 +181,7 @@ ax.plot(posi_1,perm_2[:,start], label=r"$k^{00}$", color="tab:red")
 #ax.plot(posi_1,perm_2[:,third_quarter])
 #ax.plot(posi_1,perm_2[:,end])
 
-ax.plot(posi_1, -depo_2[:,start], label=r"$j^{0}$", color="tab:blue")
+ax.plot(posi_1,depo_2[:,start], label=r"$j^{0}$", color="tab:blue")
 #ax.plot(posi_1, depo_2[:,first_quarter])
 #ax.plot(posi_1, depo_2[:,second_quarter])
 #ax.plot(posi_1, depo_2[:,third_quarter])
@@ -144,5 +190,14 @@ ax.plot(posi_1, -depo_2[:,start], label=r"$j^{0}$", color="tab:blue")
 ax.set_xlabel(r"$x$")
 #ax.set_ylabel(r"$k$")
 
-ax.legend()
-plt.savefig(fname=os.path.join(path_results,"perm_2_and_depo_1__v__posi_1.svg"), format="svg")
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$x$",
+                             y_label=r"initial parameters",
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
+
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"perm_2_and_depo_1__v__posi_1.svg"), format="svg")
+
+
