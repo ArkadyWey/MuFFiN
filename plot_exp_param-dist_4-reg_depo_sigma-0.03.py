151848ea07d16ed2e73328960df61702795e83fa
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-04-11 15:04:10 +0100

    Structure plots (thesis)

--- plot_exp_param-dist_4-reg_depo_sigma-0.03.py
+++ plot_exp_param-dist_4-reg_depo_sigma-0.03.py
@@ -70,12 +70,11 @@ plotting.save_fig(fig=fig,fname=os.path.join(path_results,"prob_density__v__depo
 
 # Plot deposition parameter with boxes
 # -----------------------    
-plotting.thesisify_pre_ax_creation()
-fig, ax = plt.subplots(1,1)
-num_nodes_list = [4]
+num_nodes_list = [4,9,16]
 colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink"]
 for t, N in enumerate(num_nodes_list):
-
+    plotting.thesisify_pre_ax_creation()
+    fig, ax = plt.subplots(1,1)
     # Plot parameter distribution
     # ----------------------------
     # Get parameters
@@ -105,7 +104,7 @@ for t, N in enumerate(num_nodes_list):
                                                   bins=bin_edges.bin_edges, 
                                                   density=True, 
                                                   align='mid', 
-                                                  alpha=0.4, color=colors[t])
+                                                  alpha=0.4, color=colors[0])
    
     
     # Interpolate histogram
@@ -114,7 +113,7 @@ for t, N in enumerate(num_nodes_list):
     spl = interpolate.splrep(bin_centres, count_param_1, k=3)
     x2 = numpy.linspace(bin_centres[0], bin_centres[-1], 10*num_bins)
     y2 = interpolate.splev(x2, spl)
-    ax.plot(x2,y2,color=colors[t], 
+    ax.plot(x2,y2,color=colors[0], 
                   linewidth=2.0, 
                   linestyle="-", 
                   alpha=1.0)
@@ -170,31 +169,31 @@ for t, N in enumerate(num_nodes_list):
                   alpha=1.0)
 
 
-# Attempt to fit a log-normal distribution
-# -------
-sigma = conf.sigma/numpy.sqrt(N)
-#for i,N in enumerate(num_nodes_list):
-#for mu in plot_depo_aprx_v_density.x_j_aprx_1:
-mu = numpy.log(conf.mean/2)-(sigma**2)/2
-#mu = conf.mu + numpy.log(conf.mean/2/N)
-x = numpy.linspace(mu-10, mu+10, 1_0000)
-# Normal
-# pdf = (numpy.exp(-(x - mu)**2 / (2 * sigma**2))  / (sigma * numpy.sqrt(2 * numpy.pi))) 
-# lognormal
-pdf = (numpy.exp(-(numpy.log(x) - mu)**2 / (2 * sigma**2))  / (x * sigma * numpy.sqrt(2 * numpy.pi)))/numpy.sqrt(N)
-#ax.plot(x, pdf, linewidth=1, linestyle="--", label=r"$\sigma={}$".format(sigma), color="tab:red")
+    ## Attempt to fit a log-normal distribution
+    ## -------
+    #sigma = conf.sigma/numpy.sqrt(N)
+    ##for i,N in enumerate(num_nodes_list):
+    ##for mu in plot_depo_aprx_v_density.x_j_aprx_1:
+    #mu = numpy.log(conf.mean/2)-(sigma**2)/2
+    ##mu = conf.mu + numpy.log(conf.mean/2/N)
+    #x = numpy.linspace(mu-10, mu+10, 1_0000)
+    ## Normal
+    ## pdf = (numpy.exp(-(x - mu)**2 / (2 * sigma**2))  / (sigma * numpy.sqrt(2 * numpy.pi))) 
+    ## lognormal
+    #pdf = (numpy.exp(-(numpy.log(x) - mu)**2 / (2 * sigma**2))  / (x * sigma * numpy.sqrt(2 * numpy.pi)))/numpy.sqrt(N)
+    ##ax.plot(x, pdf, linewidth=1, linestyle="--", label=r"$\sigma={}$".format(sigma), color="tab:red")
 
-# Cleanup graph 
-# -------------
-plotting.thesisify_post_plot(ax=ax,
-                             x_label=r"$j^{1}$",
-                             y_label=r"Probability density",
-                             x_left=0.0-0.05,
-                             x_right=conf.mean+0.05,
-                             y_bottom=0.0,
-                             y_top=None)
+    # Cleanup graph 
+    # -------------
+    plotting.thesisify_post_plot(ax=ax,
+                                 x_label=r"$j^{1}$",
+                                 y_label=r"Probability density",
+                                 x_left=0.0-0.05,
+                                 x_right=conf.mean+0.05,
+                                 y_bottom=0.0,
+                                 y_top=None)
 
-plotting.save_fig(fig=fig,fname=os.path.join(path_results,"prob_density__v__depo__with_approx.svg"))
+    plotting.save_fig(fig=fig,fname=os.path.join(path_results,"prob_density__v__depo__with_approx_N={}.svg".format(N)))
 
 
 
@@ -235,8 +234,8 @@ ax.scatter(num_nodes_list,sd_1, label=r"$\mathbb{S}[j^{1}]$")
 N_smooth =  numpy.linspace(1,100,500)
 #ax.plot(N_smooth, (mean_1[-1]-mean_1[0])*numpy.ones_like(N_smooth), color="tab:blue",ls="--")
 
-ax.plot(N_smooth, (mean_1[-1])*numpy.ones_like(N_smooth), color="tab:blue",ls="--")
 ax.plot(N_smooth, (conf.mean*conf.get_cdf(conf.mean))*numpy.ones_like(N_smooth), color="tab:blue",ls="-",label=r"$\bar{G}\mathrm{cdf}(\bar{G})$")
+ax.plot(N_smooth, (mean_1[-1])*numpy.ones_like(N_smooth), color="tab:blue",ls="--", label=r"$\bar{j}^{1}_{N \rightarrow \infty}$")
 #ax.plot(N_smooth, (conf.mean/2)*numpy.ones_like(N_smooth), color="tab:blue",ls="-",label=r"$\frac{\bar{G}}{2}$")
 ax.plot(N_smooth, 0.81873075307*numpy.power(N_smooth,-0.5), color="tab:orange", label=r"$0.819N^{-\frac{1}{2}}$",ls="-")
 # exp(-0.2)= 0.81873075307
@@ -251,7 +250,7 @@ print("pcnt:{}".format(pcnt))
 # -------------
 plotting.thesisify_post_plot(ax=ax,
                              x_label=r"$N$",
-                             y_label=None,
+                             y_label=r"Adhesivity statistics",
                              x_left=None,
                              x_right=None,
                              y_bottom=None,
@@ -287,7 +286,83 @@ plotting.save_fig(fig=fig,fname=os.path.join(path_results,"logmean-j_and_logstd-
 
 
 
+# Plot difference between mean and limit of mean 
+# ------------------------------------
+num_nodes_list = [1,4,9,16,25,36,49,64,81,100]
+num_tests = len(num_nodes_list)
+
+plotting.thesisify_pre_ax_creation()
+fig, ax = plt.subplots(1,1)
+
+# Get mean and standard deviation for each N
+# -------
+mean_1 = numpy.zeros(shape=num_tests)
+for t in range(num_tests):
+    N = num_nodes_list[t]
+
+    depo_effe_2 = numpy.load(os.path.join(path_results, "depo_effe_1_N-{}.npy".format(N)))
+    mean_1[t] = numpy.mean(a=depo_effe_2, axis=0)
+
+# Plot scatter for distribution means
+# -------
+conf = configure.Configure(num_nodes=N,
+                           initialisation=initialisation,
+                           sigma=sigma, type_alpha=type_alpha)
+#ax.scatter(num_nodes_list, abs(mean_1-mean_1[-1])/mean_1[-1], label=r"$\frac{\left|\mathbb{E}[j^{1}]-\bar{j}^{1}|_{N\rightarrow \infty}\right|}{\bar{j}^{1}|_{N\rightarrow \infty}}$")
+ax.scatter(num_nodes_list, abs(mean_1-mean_1[-1])/mean_1[-1], label=r"$E_\mathrm{c}^j$")
+
+# Plot guide lines
+# ------
+N_smooth = numpy.linspace(1.25,100,500)
+ax.plot(N_smooth, numpy.exp(-3.5)*numpy.power(N_smooth,-1.0), color="tab:blue", label=r"$0.03N^{-1}$",ls="-")
+print("numpy.exp(-1.7)",numpy.exp(-3.5))
 
 
-# TODO: 
-# Consider checking what distribution the final gistogram is similar too?
+colors = ["tab:orange","tab:green","tab:red"]
+for i,N in enumerate([4,9,16]):
+    ax.vlines(x=N, 
+              ymin=-0.0001, 
+              ymax=numpy.exp(-3.5)*numpy.power(N,-1.0), 
+              color=colors[i], 
+              linewidth=2.0, 
+              linestyle="--", 
+              alpha=1.0, 
+              label=r"$N={}$".format(N))
+    N_smooth = numpy.linspace(-0.01,N,1000)
+    ax.plot(N_smooth, numpy.exp(-3.5)*numpy.power(N,-1.0)*numpy.ones_like(N_smooth), color=colors[i],linestyle="--")
+
+
+# Cleanup graph 
+# ----
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"$N$",
+                             y_label=r"$E_\mathrm{c}^j$",
+                             x_left=-0.01,
+                             x_right=105.0,
+                             y_bottom=-0.0001,
+                             y_top=None)
+
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"mean-j_diff__v__N.svg"), format="svg")
+
+
+
+# Plot Log Log to check gradient of mean diff
+# -------
+plotting.thesisify_pre_ax_creation()
+fig, ax = plt.subplots(1,1)
+
+N_smoother = numpy.linspace(0.01,5,500)
+ax.scatter(numpy.log(num_nodes_list),numpy.log(abs(mean_1-mean_1[-1])/mean_1[-1]), label=r"log(mean $j^1$$)$")
+ax.plot(N_smoother, -1.0*N_smoother + ((-3.5)*numpy.ones_like(N_smoother)), color="tab:orange", label=r"$-\frac{1}{2}log(N)-0.697$")
+
+# Cleanup graph 
+# -------------
+plotting.thesisify_post_plot(ax=ax,
+                             x_label=r"log$(N)$",
+                             y_label=None,
+                             x_left=None,
+                             x_right=None,
+                             y_bottom=None,
+                             y_top=None)
+
+plotting.save_fig(fig=fig,fname=os.path.join(path_results,"logmean-j_diff__v__logN.svg"), format="svg")
