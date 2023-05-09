348552003055258853811cb08e80fa5b424fc0b9
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-05-09 18:37:33 +0100

    Add remote path to run_preprocess

--- run_ensemble-network.sh
+++ run_ensemble-network.sh
@@ -2,7 +2,11 @@ for r in {0..10}; do
 
 echo Running for r-$r
 
-python3 run_network.py --path_results "/home/user/projects/papers/2023_homogenisation/figures/results_ensemble-network/r-$r"; 
+# Local
+#python3 run_network.py --path_results "/home/user/projects/papers/2023_homogenisation/figures/results_ensemble-network/r-$r"; 
+
+# Remote
+python3 run_network.py --path_results "/scratch/2023_homogenisation/figures/results_ensemble-network/r-$r"; 
 
 done
 
