8a75a833e6746468ed06104b2310ccd1a5288e53
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-05-09 18:41:14 +0100

    Add remote path for preprocess ensemble

--- run_ensemble-preprocess.sh
+++ run_ensemble-preprocess.sh
@@ -2,7 +2,11 @@ for r in {0..20000}; do
 
 echo Running for r-$r
 
-python3 run_preprocess_2D.py --path_results "/home/user/projects/papers/2023_homogenisation/figures/results_ensemble-preprocess/r-$r"; 
+# Local
+#python3 run_preprocess_2D.py --path_results "/home/user/projects/papers/2023_homogenisation/figures/results_ensemble-preprocess/r-$r"; 
+
+# Remote
+python3 run_preprocess_2D.py --path_results "/scratch/2023_homogenisation/figures/results_ensemble-preprocess/r-$r"; 
 
 done
 
