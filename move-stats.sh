f8b135d2cd47c22cb9f248afc1b9acbccf698ea2
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-06-07 21:45:21 +0100

    Correct move-stats.sh

--- move-stats.sh
+++ move-stats.sh
@@ -1,6 +1,6 @@
 
-for N in 1 4 9 16 25 36 49 64 81 100 do;
-    for data_type in stats fulls do; 
+for N in 1 4 9 16 25 36 49 64 81 100; do
+    for data_type in stats fulls; do 
         mkdir -p  ~/Projects/data_to_transfer/init-4-reg/N-${N} &&  cp -r /scratch/wey/2023_homogenisation/figures/poly/esbl_prep/init-4-reg/N-${N}/${data_type}_init-4-reg_N-${N}/ ~/Projects/data_to_transfer/init-4-reg/N-1/${data_type}_init-4-reg_N-${data_type}/
     done
 done
