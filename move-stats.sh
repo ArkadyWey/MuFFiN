3df5f2f094cb46314b4943a423b7d7971b2d31fe
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-06-07 21:53:50 +0100

    Add init to move-stats.sh

--- move-stats.sh
+++ move-stats.sh
@@ -1,6 +1,7 @@
-
-for N in 1 4 9 16 25 36 49 64 81 100; do
-    for data_type in stats fulls; do 
-        mkdir -p  ~/Projects/data_to_transfer/init-4-reg/N-${N} &&  cp -r /scratch/wey/2023_homogenisation/figures/poly/esbl_prep/init-4-reg/N-${N}/${data_type}_init-4-reg_N-${N}/ ~/Projects/data_to_transfer/init-4-reg/N-${N}/${data_type}_init-4-reg_N-${N}/
+for init in 4-reg 6-reg 6-ireg; do
+    for N in 1 4 9 16 25 36 49 64 81 100; do
+        for data_type in stats fulls; do 
+            mkdir -p  ~/Projects/data_to_transfer/init-4-reg/N-${N} &&  cp -r /scratch/wey/2023_homogenisation/figures/poly/esbl_prep/init-4-reg/N-${N}/${data_type}_init-4-reg_N-${N}/ ~/Projects/data_to_transfer/init-4-reg/N-${N}/${data_type}_init-4-reg_N-${N}/
+        done
     done
 done
