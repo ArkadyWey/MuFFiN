d913d4a29a2c7cacf334537f9e76bc9d3f188c2f
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-06-08 00:31:03 +0100

    Add N-50 to move.sh

--- move.sh
+++ move.sh
@@ -1,7 +1,7 @@
-declare -i num_nodes=4   #81    #100
-#declare -i r_max=5      #694   #3106
-declare -i diff=11       #9306  #6894
-declare -i r_max_new=15  #10000 #10000
+declare -i num_nodes=4   #81    #100   #50
+#declare -i r_max=5      #694   #3106  #1513
+declare -i diff=11       #9306  #6894  #8488
+declare -i r_max_new=15  #10000 #10000 #10000
 
 for ((r=${diff};r<=${r_max_new};r+=1)); do
 	echo "Running for r-${r}"
