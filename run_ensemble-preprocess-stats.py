7cd3973f31e10754cafaa95c175049c8d952b560
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-07-20 15:43:10 +0100

    Add conductance to preprocess stats

--- run_ensemble-preprocess-stats.py
+++ run_ensemble-preprocess-stats.py
@@ -3,10 +3,10 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-pr",    "--path_results", type=str, help="Path to results")
+parser.add_argument("-pr",    "--path_results",  type=str, help="Path to results")
 parser.add_argument("-rm",    "--r_max",         type=int, help="Number of repeated simulations")
-parser.add_argument("-N",     "--num_nodes",      type=int,   help="Number of nodes in cell")
-parser.add_argument("-init",  "--initialisation", type=str,   help="Structure of cell")
+parser.add_argument("-N",     "--num_nodes",     type=int, help="Number of nodes in cell")
+parser.add_argument("-init",  "--initialisation",type=str, help="Structure of cell")
 args = parser.parse_args()
 path_results   = args.path_results
 r_max          = args.r_max 
@@ -16,6 +16,7 @@ num_nodes      = args.num_nodes
 perm_prep_4 = [] # [r_max+1,k,m,n] 
 depo_prep_3 = [] # [r_max+1,k,m]
 delt_6      = [] # [r_max+1,k,i,j,r,m]
+cond_tabl_6 = [] # [r_max+1,k,i,j,r,m]
 
 
 # Collect results in lists
@@ -24,11 +25,13 @@ for r in range(r_max+1):
     
     perm_prep_3 = numpy.load(os.path.join(path_results_r, "perm_prep_3.npy"))
     depo_prep_2 = numpy.load(os.path.join(path_results_r, "depo_prep_2.npy"))
-    delt_5 = numpy.load(os.path.join(path_results_r, "delt_5.npy"))
+    delt_5      = numpy.load(os.path.join(path_results_r, "delt_5.npy"))
+    cond_tabl_5 = numpy.load(os.path.join(path_results_r, "cond_tabl_5.npy"))
 
     perm_prep_4.append(perm_prep_3)
     depo_prep_3.append(depo_prep_2)
     delt_6.append(delt_5)
+    cond_tabl_6.append(cond_tabl_5)
 
     if r==0:
         conc_max_or_tot_1 = numpy.load(os.path.join(path_results_r, "conc_max_or_tot_1.npy"))
@@ -39,15 +42,18 @@ for r in range(r_max+1):
 perm_prep_4 = numpy.array(perm_prep_4)
 depo_prep_3 = numpy.array(depo_prep_3)
 delt_6      = numpy.array(delt_6)
+cond_tabl_6 = numpy.array(cond_tabl_6)
 
 # Calculate statistics
 perm_prep_av_3 = numpy.mean(a=perm_prep_4, axis=0)
 depo_prep_av_2 = numpy.mean(a=depo_prep_3, axis=0)
 delt_av_5      = numpy.mean(a=delt_6,      axis=0)
+cond_tabl_av_5 = numpy.mean(a=cond_tabl_6, axis=0)
 
 perm_prep_sd_3 = numpy.std(a=perm_prep_4, axis=0)
 depo_prep_sd_2 = numpy.std(a=depo_prep_3, axis=0)
 delt_sd_5      = numpy.std(a=delt_6,      axis=0)
+cond_tabl_sd_5 = numpy.std(a=cond_tabl_6, axis=0)
 
 # Save statistics
 #path_results = "/home/user/projects/papers/2023_homogenisation/figures/results_ensemble-preprocess/stats"
@@ -62,12 +68,14 @@ numpy.save(file=os.path.join(path_stats,"conc_max_or_tot_1.npy"), arr=conc_max_o
 numpy.save(file=os.path.join(path_stats,"perm_prep_av_3.npy"), arr=perm_prep_av_3, allow_pickle=True, fix_imports=True)
 numpy.save(file=os.path.join(path_stats,"depo_prep_av_2.npy"), arr=depo_prep_av_2, allow_pickle=True, fix_imports=True)
 numpy.save(file=os.path.join(path_stats,"delt_av_5.npy"),      arr=delt_av_5,      allow_pickle=True, fix_imports=True)
+numpy.save(file=os.path.join(path_stats,"cond_tabl_av_5.npy"), arr=cond_tabl_av_5, allow_pickle=True, fix_imports=True)
 
 
 # Save stds
 numpy.save(file=os.path.join(path_stats,"perm_prep_sd_3.npy"), arr=perm_prep_sd_3, allow_pickle=True, fix_imports=True)
 numpy.save(file=os.path.join(path_stats,"depo_prep_sd_2.npy"), arr=depo_prep_sd_2, allow_pickle=True, fix_imports=True)
 numpy.save(file=os.path.join(path_stats,"delt_sd_5.npy"),      arr=delt_sd_5,      allow_pickle=True, fix_imports=True)
+numpy.save(file=os.path.join(path_stats,"cond_tabl_sd_5.npy"), arr=cond_tabl_sd_5, allow_pickle=True, fix_imports=True)
 
 
 # Save entire results (for distributions)
@@ -78,3 +86,4 @@ if not os.path.exists(path_fulls):
 numpy.save(file=os.path.join(path_fulls,"perm_prep_4.npy"), arr=perm_prep_4, allow_pickle=True, fix_imports=True)
 numpy.save(file=os.path.join(path_fulls,"depo_prep_3.npy"), arr=depo_prep_3, allow_pickle=True, fix_imports=True)
 numpy.save(file=os.path.join(path_fulls,"delt_6.npy"),      arr=delt_6,      allow_pickle=True, fix_imports=True)
+numpy.save(file=os.path.join(path_fulls,"cond_tabl_6.npy"), arr=cond_tabl_6, allow_pickle=True, fix_imports=True)
