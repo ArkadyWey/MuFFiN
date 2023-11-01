a1b03429ca3aabf04af620edd62235692e523b4f
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-11-01 12:04:20 +0000

    Move *.sh to ./bash/

--- move-stats.sh
+++
@@ -1,7 +0,0 @@
-declare init=4-reg
-
-for N in 1 4 9 16 25 36 49 64 81 100; do
-    for data_type in stats fulls; do 
-        mkdir -p  ~/Projects/data_to_transfer/init-${init}/N-${N} &&  cp -r /scratch/wey/2023_homogenisation/figures/poly/esbl_prep/init-${init}/N-${N}/${data_type}_init-${init}_N-${N}/ ~/Projects/data_to_transfer/init-${init}/N-${N}/${data_type}_init-${init}_N-${N}/
-    done
-done
