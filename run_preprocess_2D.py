e1f49327f2e504cf1963854f86109736c6b274eb
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-11-02 15:55:41 +0000

    Remove unneeded comments

--- run_preprocess_2D.py
+++ run_preprocess_2D.py
@@ -1,17 +1,21 @@
 import numpy
 import os
 import datetime
-import matplotlib.pyplot as plt
 import argparse
-
 
 import multiscale_models.configure as configure
 import multiscale_models.preprocess_2D as preprocess_2D
 import multiscale_models.preprocess_blocking_2D as preprocess_blocking_2D
 import multiscale_models.preprocess_deposition_2D as preprocess_deposition_2D
 
-
-def main(num_nodes: int, initialisation: str, sigma: float, type_alpha: str, type_clog: str, path_cond_init_4: str, alph: int, beta: int):
+def main(num_nodes: int, 
+         initialisation: str, 
+         sigma: float, 
+         type_alpha: str, 
+         type_clog: str, 
+         path_cond_init_4: str, 
+         alph: int, 
+         beta: int):
     """
     """
 
@@ -24,20 +28,11 @@ def main(num_nodes: int, initialisation: str, sigma: float, type_alpha: str, typ
                                path_cond_init_4=path_cond_init_4)
     
     conc_max_or_tot_1 = conf.conc_max_or_tot_1 
-    #a = numpy.linspace(0.0,0.85,11)
-    #b = numpy.linspace(0.85,0.95,101-10-10)
-    #c = numpy.linspace(0.95,1.0,11)
-    #a = numpy.arange(0.0,0.88,0.01)
-    #b = numpy.arange(0.88,0.92,0.0001)
-    #c = numpy.arange(0.92,1.01,0.01)
-    #conc_max_or_tot_1 = numpy.concatenate((a,b,c))
-    #print(len(conc_max_or_tot_1))
-    #print(conc_max_or_tot_1)
-    cond_init_4     = conf.cond_init_4 
-    adhe_init_4     = alph*conf.adhe_init_4 
-    alpha           = conf.alpha 
-    refs_2          = conf.refs_2 
-    leng_1          = conf.leng_1
+    cond_init_4       = conf.cond_init_4 
+    adhe_init_4       = alph*conf.adhe_init_4 
+    alpha             = conf.alpha 
+    refs_2            = conf.refs_2 
+    leng_1            = conf.leng_1
 
 
     if type_clog == "block":
@@ -45,33 +40,21 @@ def main(num_nodes: int, initialisation: str, sigma: float, type_alpha: str, typ
                                                                                          cond_init_4=cond_init_4, 
                                                                                          adhe_init_4=adhe_init_4, 
                                                                                          alpha=alpha)
-        #r = 0
-        #m = 0
-        #print("avrg cond:",numpy.mean(cond_tabl_5))
-        #print("cond_tabl_5[0,:,:,0,0]: \n{}".format(cond_tabl_5[0,:,:,r,0]))
-        #print("adhe_tabl_5[0,:,:,0,0]: \n{}".format(adhe_tabl_5[0,:,:,0,0]))
-
 
 
         lhs_3, rhs_4 = preprocess_blocking_2D.get_cell_problem(cond_tabl_5=cond_tabl_5, 
                                                                refs_2=refs_2, 
                                                                leng_1=leng_1)
 
-        #print("rhs_4[0,:,:,0]: \n{}".format(rhs_4[0,:,:,0]))
-        #print("lhs_3[0,:,:]: \n{}".format(lhs_3[0,:,:]))
-
-
 
         csol_3 = preprocess_blocking_2D.get_cell_solution(lhs_3=lhs_3, 
                                                           rhs_4=rhs_4)
-        #print("csol_3[0,:,0]: \n{}".format(csol_3[0,:,:]))
 
 
 
         delt_5 = preprocess_blocking_2D.get_delta(csol_3=csol_3, 
                                          refs_2=refs_2, 
                                          leng_1=leng_1)
-        #print("delt_5[0,:,:,0,0]: \n{}".format(delt_5[-1,:,:,-1,0]))
 
     elif type_clog == "deposit":
         cond_tabl_5,adhe_tabl_5,csol_3,delt_5 = preprocess_deposition_2D.get_conductance_adherence_csol_delta(conc_tot_disc_1=conc_max_or_tot_1,
@@ -83,20 +66,8 @@ def main(num_nodes: int, initialisation: str, sigma: float, type_alpha: str, typ
                                                                                                               alph=alph)
     else: 
         raise Exception("type_clog must be either 'block' or 'deposit'.")
-        #print(csol_3[0:2,:,:])
-        #plt.plot(conc_max_or_tot_1,cond_tabl_5[:,0,1,0,0])
-        #plt.plot(conc_max_or_tot_1,1/((1/1)+(conc_max_or_tot_1/2))**2,ls="--")
-        #plt.show()
 
     heav_5 = preprocess_2D.get_heaviside(delt_5=delt_5)
-    #print("heav_5[0,:,:,0,0]: \n{}".format(heav_5[0,:,:,0,0]))
-    
-    #print(-numpy.mean( a=(-delt_5[-1,:,:,0,0]), axis=None))
-
-
-
-    #print(-numpy.mean( a=-delt_5[-1,:,:,r,m]*(heav_5[-1,:,:,r,m]), axis=None))
-    #print(-delt_5[-1,:,:,r,m]*(heav_5[-1,:,:,r,m]))
 
     perm_3, depo_2 = preprocess_2D.get_permeability_and_deposition(refs_2=refs_2,
                                                                    cond_tabl_5=cond_tabl_5,
@@ -112,9 +83,6 @@ def main(num_nodes: int, initialisation: str, sigma: float, type_alpha: str, typ
     #print("depo_2[:,1]: \n{}".format(depo_2[-1,1]))
     #print("delt_5[:,i,j,r,m]: \n{}".format(delt_5[0,:,:,-1,0]))
     #print("heav_5[:,i,j,r,m]: \n{}".format(heav_5[0,:,:,-1,0]))
-
-    #if depo_2[-1,0] > 4.0:
-    #    exit()
     return (perm_3, depo_2, conc_max_or_tot_1, cond_tabl_5, adhe_tabl_5, delt_5, heav_5)
 
 def get_path_to_initial_conductance():
@@ -153,8 +121,8 @@ if __name__ == "__main__":
     type_clog  = "deposit"
     path_cond_init_4 = get_path_to_initial_conductance()
 
-    alph = args.alph #1.0#1.0#0.3#0.35#0.5#0.3333333333333333#0.5#0.5#1.0#0.1#0.5 (epsi*delt**2)
-    beta = args.beta #0.01#1.0#1.0#10#20.0
+    alph = args.alph #1.0
+    beta = args.beta #0.01
 
     if not os.path.exists(path_results):
         os.makedirs(path_results)
@@ -176,11 +144,8 @@ if __name__ == "__main__":
     end_time = datetime.datetime.now()
     print("sim_time:\n {}".format(end_time-begin_time))
 
-
-
     # Save results 
     # ----- 
-
     numpy.save(file=os.path.join(path_results,"perm_prep_3.npy"),       arr=perm_prep_3,     allow_pickle=True, fix_imports=True)
     numpy.save(file=os.path.join(path_results,"depo_prep_2.npy"),       arr=depo_prep_2,     allow_pickle=True, fix_imports=True)
     numpy.save(file=os.path.join(path_results,"conc_max_or_tot_1.npy"), arr=conc_max_or_tot_1, allow_pickle=True, fix_imports=True)
