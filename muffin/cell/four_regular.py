eb9b3edc6922600f0ab5cd03996df2549b6ccbbe
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-11-06 22:57:37 +0000

    Add four_regular cell class

---
+++ muffin/cell/four_regular.py
@@ -0,0 +1,167 @@
+import numpy 
+import scipy
+
+import muffin.initial_conditions.distributions as distributions
+
+class Four_Regular():
+    """
+    """
+    def __init__(self, num_nodes:int, 
+                       dist_cond:str,
+                       dist_adhe:str,
+                       mu:float, 
+                       sigma:float, 
+                       num_refs:int=3):
+        """
+        """
+
+        # Parameters
+        self.num_nodes:int = num_nodes
+        self.n:int         = int(numpy.sqrt(num_nodes)) # number of rows or cols in square cell
+        self.num_refs:int  = num_refs
+
+        # Sampling
+        self.dist_cond:str = dist_cond
+        self.dist_adhe:str = dist_adhe
+        self.mu:float      = mu
+        self.sigma:float   = sigma
+
+        # Execute methods
+        self.conn_4:numpy.ndarray = self.make_conn_4()
+        self.cond_4:numpy.ndarray = self.fill_edges(dist="lognormal")
+        self.adhe_4:numpy.ndarray = self.fill_edges(dist="ones")
+
+
+    def get_sample(self, dist):
+        
+        if dist == "ones":
+            sample_1 = distributions.ones(num_samples=1)
+
+        elif dist == "lognormal":
+            sample_1 = distributions.lognormal(mu=self.mu, 
+                                              sigma=self.sigma, 
+                                              num_samples=1)
+        else:
+            raise Exception("""A function corresponding to this distribution choice does not exist. \ 
+                               See muffin.initial_conditions.distributions""")
+        return sample_1
+
+
+    def fill_edges(self, dist):
+        """
+        """
+        a_4 = numpy.zeros_like(self.conn_4)
+        for r in [0,1,-1]:
+            for s in [0,1,-1]:
+                for i in numpy.arange(start=0,stop=self.num_nodes,step=1):
+                    for j in numpy.arange(start=i,stop=self.num_nodes,step=1):
+                        if self.conn_4[i,j,r,s] != 0.0 and a_4[i,j,r,s] == 0:
+                            sample_1 = self.get_sample(dist=dist)
+                            a_4[i,j,r,s]   = sample_1[0]
+                            a_4[j,i,-r,-s] = sample_1[0]
+        return a_4
+
+    def make_conn_4(self):
+        """
+        Get cell initial connectivity tensor.
+
+        Parameters
+        -----
+        - num_nodes: int
+            Number of nodes in the cell. Must be a square number.
+        - num_refs: int
+            Number of lengths in the reference set. 
+            For example, if reference set is {-1,0,+1} then num_refs==3.
+        - mu: float 
+            Mean of the normal distribution from which the lognormal distribution is derived. 
+            Must be non-negative. 
+        - sigma: float: 
+            Standard deviation of the normal distribution from which the lognormal distribution is derived. 
+            Must be non-negative. 
+    
+        Returns 
+        -----
+        - conn_4: numpy.ndarray
+            Connectivity tensor of the cell.
+            conn_4[i,j,r1,r2] = 1 if edge exists between node i in reference cell and node j in the cell 
+            at position r1,r2.
+
+        """
+        
+        # Get parameters
+        # -----
+        num_nodes = self.num_nodes
+        num_refs  = self.num_refs
+        n         = self.n
+        
+        # Make empty connectivity tensor
+        # -----
+        conn_4 = numpy.zeros(shape=(num_nodes, num_nodes, num_refs, num_refs))
+
+        # Add intra-cell edges
+        # -----
+        # Form block
+        diagonals = [numpy.ones(n-1), numpy.ones(n-1)]
+        a_2 = scipy.sparse.diags(diagonals=diagonals, offsets=[1,-1], shape=(n, n)).toarray()
+
+        # Form block diagonal
+        tup = n*(a_2,)
+        b_2 = scipy.linalg.block_diag(*tup)
+
+        # Form outer diagonals
+        more_diagonals = [numpy.ones(num_nodes-n), numpy.ones(num_nodes-n)]
+        c_2 = scipy.sparse.diags(diagonals=more_diagonals, offsets=[n, -n], shape=(num_nodes, num_nodes)).toarray()
+
+        # Form adjacency matrix
+        adj_intra_2 = b_2 + c_2
+
+        # Add intra-cell edges to cell
+        conn_4[:,:,0,0] = adj_intra_2
+
+
+        # Add inter-cell edges
+        # -----
+
+        # Get all node indices
+        nodes = numpy.linspace(0,n**2-1,n**2)
+
+        # Get indices of nodes on outside of cell
+        left_nodes   = nodes[0::n]
+        right_nodes  = nodes[n-1::n]
+        top_nodes    = nodes[0:n]
+        bottom_nodes = nodes[self.num_nodes-n::]
+
+        # Add inter-cell edges to cell
+        for i in range(n):
+            left_node   = int(left_nodes[i])
+            right_node  = int(right_nodes[i])
+            top_node    = int(top_nodes[i])
+            bottom_node = int(bottom_nodes[i])
+ 
+            conn_4[left_node,right_node,-1,0] = 1 
+            conn_4[right_node,left_node,+1,0] = 1 
+            conn_4[bottom_node,top_node,0,-1] = 1 
+            conn_4[top_node,bottom_node,0,+1] = 1
+
+        return conn_4
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    cell = Four_Regular(num_nodes=4, 
+                            dist_cond="lognormal",
+                            dist_adhe="ones",
+                            mu=0.5, 
+                            sigma=0.3, 
+                            num_refs=3)
+
+    for i in range(4):
+        for j in range(4):
+            for r in [0,1,-1]:
+                for s in [0,1,-1]:
+                    if cell.cond_4[i,j,r,s] != cell.cond_4[j,i,-r,-s]:
+                        raise Exception()
