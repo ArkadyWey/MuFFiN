c9b9cd9cb242f1d9903a43bd56b57e148cef9930
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-11-07 12:46:40 +0000

    Add sample function wrapping
    - Now user supplies initial
    condition distribution name
    and *kwargs
    and wrapper chooses which sampler to
    use to return sample

--- muffin/initial_conditions/distributions.py
+++ muffin/initial_conditions/distributions.py
@@ -1,11 +1,57 @@
 import numpy
+import inspect
+import types
 
-def ones(num_samples:int):
+# Define different initial condition distributions 
+# -----
+
+def delta(mu:float):
     """
     """
-    return numpy.ones(num_samples)
+    sample = mu
+    return sample
     
-def lognormal(num_samples:int, mu:float, sigma:float):
+def lognormal(mu:float, sigma:float):
     """
     """
-    return numpy.random.lognormal(mean=mu, sigma=sigma, size=num_samples)
+    sample = numpy.random.lognormal(mean=mu, sigma=sigma)
+    return sample
+
+
+# Utils
+# -----
+def get_functions():
+    functions_1 = [function for function in globals().values() if type(function) == types.FunctionType]
+    return functions_1
+
+def map_function_name_to_args():
+    functions_1 = get_functions() # local functions list
+    name_to_args = {func.__name__: inspect.getfullargspec(func).args for func in functions_1}
+    return name_to_args
+
+def map_function_name_to_func():
+    functions_1 = get_functions() # local functions list
+    name_to_func = {func.__name__: func for func in functions_1}
+    return name_to_func
+
+def get_sample(dist:str, **kwargs):
+    name_to_args = map_function_name_to_args()
+    name_to_func = map_function_name_to_func()
+    
+    if dist not in name_to_args.keys():
+        raise Exception("{} is not an implemented distribution.".format(dist))
+    else :
+        # Execute function with parameters 
+        func = name_to_func[dist]
+        sample = func(**kwargs)
+        return sample
+    
+
+if __name__ == "__main__": 
+
+    sample = get_sample(dist="lognormal", mu=0.5, sigma=0.3)
+    print(sample)
+
+
+
+
