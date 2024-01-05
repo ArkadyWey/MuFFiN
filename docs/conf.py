61d321c17ea9f2da96488a198a949b55797ccb57
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2024-01-05 18:17:10 +0000

    Correct html logo

--- docs/conf.py
+++ docs/conf.py
@@ -42,19 +42,19 @@ html_favicon = "./_static/favicon.ico"
 html_logo    = "./_static/logo.png",
 
 
-html_theme_options = {
-   # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-   # 'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+# html_theme_options = {
+#    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+#    # 'analytics_anonymize_ip': False,
+#     'logo_only': False,
+#     'display_version': True,
+#     'prev_next_buttons_location': 'bottom',
+#     'style_external_links': False,
+#     'vcs_pageview_mode': '',
+#     'style_nav_header_background': 'white',
+#     # Toc options
+#     'collapse_navigation': True,
+#     'sticky_navigation': True,
+#     'navigation_depth': 4,
+#     'includehidden': True,
+#     'titles_only': False
+# }
