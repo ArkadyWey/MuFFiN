69e357d7b7cae2e457aa09b4d65bfd864b87d60b
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2024-01-10 14:38:04 +0000

    Update example_preprocess to test parameters usage

--- muffin/example_preprocess.py
+++ muffin/example_preprocess.py
@@ -1,17 +1,24 @@
+import muffin.parameters.parameters as parameters
 
+# parameters = muffin.parameters.Parameters()
+# cell = muffin.cells.FourRegular(parameters)
+# equations_preprocess = muffin.equations.preprocess.deposition_flux_dependent.Deposition_FluxDependent(parameters)
 
-parameters = muffin.parameters.Parameters()
-cell = muffin.cells.FourRegular(parameters)
-equations_preprocess = muffin.equations.preprocess.deposition_flux_dependent.Deposition_FluxDependent(parameters)
+# problem = muffin.problem.Problem(parameters, cell,equations_preprocess)
 
-problem = muffin.problem.Problem(parameters, cell,equations_preprocess)
+# problem.solve(problem_type="preprocess")
 
-problem.solve(problem_type="preprocess")
+# solution = problem.solution(problem_type="preprocess")
 
-solution = problem.solution(problem_type="preprocess")
+# solution.plot() # plot all variables against s
 
-solution.plot() # plot all variables against s
+# pressure = solution.pressure
 
-pressure = solution.pressure
+# pressure.plot()
 
-pressure.plot()
+# problem.save() # save sims and plots, just sims, or just plots
+
+if __name__ == "__main__":
+
+    parameters = parameters.Parameters()
+
