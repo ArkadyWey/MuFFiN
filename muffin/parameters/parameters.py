09daddeaece441a817f55f9b0618f5e5a7192b92
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2024-01-11 13:16:40 +0000

    Add dist_cond and dist_adhe as parameters
    - NB in parser these must be parsed as
    strings.
    So implemented type conversion to dicts
    in case where they are parsed.

--- muffin/parameters/parameters.py
+++ muffin/parameters/parameters.py
@@ -15,9 +15,10 @@ class Parameters():
                        max_ref_dist:int   = 1,
                        num_dims:int       = 2,
                        num_concs:int      = 1001,
+                       num_refs           = 3,
                        v:float            = 0.5,  
-                       mu:float           = -0.045, 
-                       sigma:float        = 0.3, 
+                       dist_cond:dict     = {"name":"lognormal", "mu":-0.045, "sigma":0.3},
+                       dist_adhe:dict     = {"name":"delta",     "mu":-0.045}
                 ):
         """Add parameters as attributes and save dictionary of parameters at path.
 
@@ -50,14 +51,14 @@ class Parameters():
             # TODO: Improve explanation.
         v : float, optional
             # TODO: Check definition, by default 0.5
-        mu : float, optional
-            Initial distribution parameter. 
-            Mean of the normal distribution corresponding to the log-normal conductance/adherence 
-            distribution, by default -0.045.
-        sigma : float, optional
-            Initial distribution parameter. 
-            Standard deviation of the normal distribution corresponding to the log-normal conductance/adherence 
-            distribution, by default 0.3.
+        dict_cond : dict
+            Initial conductance distribution. Dictionary with key "name" as well as keys for any 
+            parameters of the named distribution. See muffin/initial_conditions/initial_conditions.py 
+            for options.
+        dict_adhe : dict
+            Initial adherence distribution. Dictionary with key "name" as well as keys for any 
+            parameters of the named distribution. See muffin/initial_conditions/initial_conditions.py 
+            for options.
         """
 
         # Get parameters given in class instance
@@ -80,9 +81,10 @@ class Parameters():
         self.max_ref_dist:int   = self.dictionary["max_ref_dist"]
         self.num_dims:int       = self.dictionary["num_dims"]
         self.num_concs:int      = self.dictionary["num_concs"]
+        self.num_refs:int       = self.dictionary["num_refs"]
         self.v:float            = self.dictionary["v"]
-        self.mu:float           = self.dictionary["mu"]
-        self.sigma:float        = self.dictionary["sigma"]
+        self.dist_cond:dict     = self.dictionary_class["dist_cond"]
+        self.dist_adhe:dict     = self.dictionary_class["dist_adhe"]
 
     # Do
     # -----
@@ -103,22 +105,30 @@ class Parameters():
         parser = argparse.ArgumentParser()
             
         # Optionally add parameters from parser
-        parser.add_argument("-pr",   "--path",           type=str,     required=False, help="Path to results")
-        parser.add_argument("-N",    "--num_nodes",      type=int,     required=False, help="Number of nodes in cell")
-        parser.add_argument("-init", "--initialisation", type=str,     required=False, help="Structure of cell")
-        parser.add_argument("-a",    "--alph",           type=float,   required=False, help="Adherence parameter")
-        parser.add_argument("-b",    "--beta",           type=float,   required=False, help="Reaction parameter")
-        parser.add_argument("-mrd",  "--max_ref_dist",   type=float,   required=False, help="Maximum number of cells between which adjacent nodes are connected")
-        parser.add_argument("-nd",   "--num_dims",       type=float,   required=False, help="Number of dimensions")
-        parser.add_argument("-nc",   "--num_concs",      type=float,   required=False, help="Number of mass flux points to discretise with, by default 1001")
-        parser.add_argument("-v",    "--v",              type=float,   required=False, help="Check definition")
-        parser.add_argument("-mu",   "--mu",             type=float,   required=False, help="Mean")
-        parser.add_argument("-sig",  "--sigma",          type=float,   required=False, help="Standard deviation")
+        parser.add_argument("-pr",   "--path",           type=str,   required=False, help="Path to results")
+        parser.add_argument("-N",    "--num_nodes",      type=int,   required=False, help="Number of nodes in cell")
+        parser.add_argument("-init", "--initialisation", type=str,   required=False, help="Structure of cell")
+        parser.add_argument("-a",    "--alph",           type=float, required=False, help="Adherence parameter")
+        parser.add_argument("-b",    "--beta",           type=float, required=False, help="Reaction parameter")
+        parser.add_argument("-mrd",  "--max_ref_dist",   type=float, required=False, help="Maximum number of cells between which adjacent nodes are connected")
+        parser.add_argument("-nd",   "--num_dims",       type=float, required=False, help="Number of dimensions")
+        parser.add_argument("-nc",   "--num_concs",      type=float, required=False, help="Number of mass flux points to discretise with, by default 1001")
+        parser.add_argument("-v",    "--v",              type=float, required=False, help="Check definition")
+        parser.add_argument("-nr",   "--num_refs",       type=int,   required=False, help="Number of cell reference numbers")
+        parser.add_argument("-dc",   "--dist_cond",      type=str,   required=False, help="Conductance distribution")
+        parser.add_argument("-da",   "--dist_adhe",      type=str,   required=False, help="Adherence distribution")
         
         # Remove nones from parameters not given
         parser_args = parser.parse_args() 
         dict_parser_args = vars(parser_args) # convert args to dictionary
         dictionary_parser = load_and_save.remove_none_items_from_dict(d=dict_parser_args) # remove nones
+        
+        # Convert distribution strings to dictionaries if given in parser
+        if "dist_cond" in dictionary_parser.keys():
+            dictionary_parser["dist_cond"] = json.loads(dictionary_parser["dist_cond"])
+        if "dist_adhe" in dictionary_parser.keys():
+            dictionary_parser["dist_adhe"] = json.loads(dictionary_parser["dist_adhe"])
+        
         return dictionary_parser
 
 
