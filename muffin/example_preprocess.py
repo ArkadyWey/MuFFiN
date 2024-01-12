2d7b861862116f7f48f0fd13ec345b5fc2dcf7b9
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2024-01-12 10:04:44 +0000

    Test problem classes

--- muffin/example_preprocess.py
+++ muffin/example_preprocess.py
@@ -2,8 +2,10 @@ import muffin.parameters.parameters as parameters
 import muffin.cells.four_regular as four_regular
 import muffin.cells.six_regular as six_regular
 import muffin.cells.six_irregular as six_irregular
+import muffin.equations_preprocess.equations_preprocess as equations_preprocess
 
 # parameters = muffin.parameters.Parameters()
+
 # cell = muffin.cells.FourRegular(parameters)
 # equations_preprocess = muffin.equations.preprocess.deposition_flux_dependent.Deposition_FluxDependent(parameters)
 
@@ -25,6 +27,19 @@ if __name__ == "__main__":
 
     parameters = parameters.Parameters()
 
-    cell = six_irregular.SixIrregular(parameters=parameters)
+    cell = four_regular.FourRegular(parameters=parameters)
 
+    equations = equations_preprocess.Deposition(parameters=parameters, cell=cell)
 
+    # solver = solvers.Explicit()
+
+    # model = models.Model(parameters, cell, equations, solver)
+
+    # model.solver.solve()
+
+    # model.solution.plot()
+
+    # model.solution.save()
+
+    # model.solution.load()
+    
