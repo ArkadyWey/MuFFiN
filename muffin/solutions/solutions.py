a6fb92ab1b8ccd16322d60573cfef7c9ad98965a
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2024-01-24 22:30:00 +0000

    Add flow solution

--- muffin/solutions/solutions.py
+++ muffin/solutions/solutions.py
@@ -5,7 +5,55 @@ import muffin.parameters.parameters as parameters
 import muffin.utils.load_and_save as load_and_save
 
 
-class Solution():
+
+class Base():
+    """_summary_
+    """
+    def __init__(self, parameters:parameters.Parameters,
+                       ): 
+        """_summary_
+        """
+
+    # Attributes
+    # -----
+        # Load variables from parameters 
+        # ------
+    
+    # Methods 
+    # -----
+    def save(self, d:dict=None, path_file:str=None):
+        load_and_save.save_dict_of_array(d=d, path_file=path_file)
+
+
+    def save_single(self, variable_name:str="conductance"):
+        y_meta = self.dictionary[variable_name]
+        self.save(d=y_meta, path_file=os.path.join(self.path_save,"{}.npy".format(y_meta["name"])))
+
+
+    def save_all(self):
+        for variable_name in self.variable_names:
+            self.save_single(variable_name=variable_name)
+
+
+    def load(self, path_file:str=None):
+        d = load_and_save.load_dict_of_array(path_file=path_file)
+        return d
+
+
+    def load_single(self, variable_name:str="conductance"):
+        d = self.load(path_file=os.path.join(self.path_save,"{}.npy".format(variable_name)))
+        return d
+    
+
+    def load_all(self):
+        self.dictionary = {}
+        for variable_name in self.variable_names:
+            d = self.load_single(variable_name=variable_name)
+            self.dictionary[variable_name] = d
+
+
+
+class Solution(Base):
     """_summary_
     """
     def __init__(self, parameters:parameters.Parameters,
@@ -33,7 +81,7 @@ class Solution():
 
         # Define solution variables to fill
         # ------
-        S = parameters.num_concs
+        S = parameters.num_tliks
         N = parameters.num_nodes
         R = parameters.num_refs
         D = parameters.num_dims
@@ -102,32 +150,116 @@ class Solution():
         self.dictionary["adhesivity"]["indices"] = ["s","m"]
 
 
-    def save(self, d:dict=None, path_file:str=None):
-        load_and_save.save_dict_of_array(d=d, path_file=path_file)
+
+class Solution_Flow(Base):
+    """_summary_
+    """
+    def __init__(self, parameters:parameters.Parameters,
+                       ): 
+        """_summary_
+        """
+
+    # Attributes
+    # -----
+        # Load variables from parameters 
+        # ------
+        self.parameters = parameters
+        self.path_save = os.path.join(self.parameters.path, "solution_flow")
+        load_and_save.check_and_make_dir(path=self.path_save)
+
+        self.variable_names = [
+            "time",
+            "position",
+            "concentration",
+            "time_like",
+            "permeability",
+            "adhesivity",
+            "velocity",
+            "pressure_gradient",
+            "reactivity"
+        ]
+
+        # Define solution variables to fill
+        # ------
+        T = parameters.num_times_solv
+        X = parameters.num_posis
+
+        self.time_solv_1 = parameters.time_solv_1
+        self.time_save_1 = parameters.time_save_1
+        self.posi_1 = parameters.posi_1
+        self.conc_2 = numpy.empty(shape=(T,X))
+        self.tlik_2 = numpy.empty(shape=(T,X))
+        self.perm_2 = numpy.empty(shape=(T,X))
+        self.depo_2 = numpy.empty(shape=(T,X))
+        self.velo_1 = numpy.empty(shape=(T))
+        self.dpdx_2 = numpy.empty(shape=(T,X))
+        self.psi_2  = numpy.empty(shape=(T,X))
 
 
-    def save_single(self, variable_name:str="conductance"):
-        y_meta = self.dictionary[variable_name]
-        self.save(d=y_meta, path_file=os.path.join(self.path_save,"{}.npy".format(y_meta["name"])))
-
-
-    def save_all(self):
-        for variable_name in self.variable_names:
-            self.save_single(variable_name=variable_name)
-
-
-    def load(self, path_file:str=None):
-        d = load_and_save.load_dict_of_array(path_file=path_file)
-        return d
-
-
-    def load_single(self, variable_name:str="conductance"):
-        d = self.load(path_file=os.path.join(self.path_save,"{}.npy".format(variable_name)))
-        return d
-    
-
-    def load_all(self):
+    # Methods
+    # ------
+    def get_dictionary(self):
         self.dictionary = {}
-        for variable_name in self.variable_names:
-            d = self.load_single(variable_name=variable_name)
-            self.dictionary[variable_name] = d
+
+        self.dictionary["time"] = {}
+        self.dictionary["time"]["name"] = "time"
+        self.dictionary["time"]["variable"] = "time_1"
+        self.dictionary["time"]["label"] = r"$t$"
+        self.dictionary["time"]["value"] = self.time_save_1
+        self.dictionary["time"]["indices"] = ["i_t"]
+
+        self.dictionary["position"] = {}
+        self.dictionary["position"]["name"] = "position"
+        self.dictionary["position"]["variable"] = "posi_1"
+        self.dictionary["position"]["label"] = r"$x$"
+        self.dictionary["position"]["value"] = self.posi_1
+        self.dictionary["position"]["indices"] = ["i_x"]
+  
+        self.dictionary["concentration"] = {}
+        self.dictionary["concentration"]["name"] = "concentration"
+        self.dictionary["concentration"]["variable"] = "conc_2"
+        self.dictionary["concentration"]["label"] = r"$c$"
+        self.dictionary["concentration"]["value"] = self.conc_2
+        self.dictionary["concentration"]["indices"] = ["i_t","i_x"]
+
+        self.dictionary["time_like"] = {}
+        self.dictionary["time_like"]["name"] = "time_like"
+        self.dictionary["time_like"]["variable"] = "tlik_2"
+        self.dictionary["time_like"]["label"] = r"$s$"
+        self.dictionary["time_like"]["value"] = self.tlik_2
+        self.dictionary["time_like"]["indices"] = ["i_t","i_x"]
+    
+        self.dictionary["permeability"] = {}
+        self.dictionary["permeability"]["name"] = "permeability"
+        self.dictionary["permeability"]["variable"] = "perm_2"
+        self.dictionary["permeability"]["label"] = r"$k$"
+        self.dictionary["permeability"]["value"] = self.perm_2
+        self.dictionary["permeability"]["indices"] = ["i_t","i_x"]
+
+        self.dictionary["adhesivity"] = {}
+        self.dictionary["adhesivity"]["name"] = "adhesivity"
+        self.dictionary["adhesivity"]["variable"] = "depo_2"
+        self.dictionary["adhesivity"]["label"] = r"$j$"
+        self.dictionary["adhesivity"]["value"] = self.depo_2
+        self.dictionary["adhesivity"]["indices"] = ["i_t","i_x"]
+
+        self.dictionary["velocity"] = {}
+        self.dictionary["velocity"]["name"] = "velocity"
+        self.dictionary["velocity"]["variable"] = "velo_1"
+        self.dictionary["velocity"]["label"] = r"$u$"
+        self.dictionary["velocity"]["value"] = self.velo_1
+        self.dictionary["velocity"]["indices"] = ["i_t"]
+
+        self.dictionary["pressure_gradient"] = {}
+        self.dictionary["pressure_gradient"]["name"] = "pressure_gradient"
+        self.dictionary["pressure_gradient"]["variable"] = "dpdx_2"
+        self.dictionary["pressure_gradient"]["label"] = r"$\partial p/\partial x$"
+        self.dictionary["pressure_gradient"]["value"] = self.dpdx_2
+        self.dictionary["pressure_gradient"]["indices"] = ["i_t","i_x"]
+
+        self.dictionary["reactivity"] = {}
+        self.dictionary["reactivity"]["name"] = "reactivity"
+        self.dictionary["reactivity"]["variable"] = "psi_2"
+        self.dictionary["reactivity"]["label"] = r"$\psi$"
+        self.dictionary["reactivity"]["value"] = self.psi_2
+        self.dictionary["reactivity"]["indices"] = ["i_t","i_x"]
